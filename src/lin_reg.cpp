636f5ca55698fcbd31fda45dccc8533ab9df3905
Grant Adams <adamsgd@uw.edu>
2018-04-17 19:01:58 -0700

    cpp changes
    changed likelihood around and included year indices

--- src/lin_reg.cpp
+++ src/lin_reg.cpp
@@ -26,24 +26,18 @@ template<class Type>
   int n_data = Y.size();
 
 
-
-  std::cout << "max of integers is " << max2(ints) << " and the code is broken \n";
-
-  int imax = max2(ints);
-  array<Type> xcheck(n_data, imax, 10);
-
-  return EXIT_FAILURE;
-
   vector<Type> mu(n_data);
   vector<Type> x2(n_data);
 
-  if(x2(1) == 1){
-    std::cout << "x2 is " << sum(x2) << " and the code is broken \n";
-    return EXIT_FAILURE;
-  }
   x2.setZero();
   x2 += x.col(0).col(0);
-  mu = (x2 * x.col(1).col(1));
+
+  if(x2(1) < 1){
+    error("Unknown fleet code");
+    return 0;
+  }
+
+  mu = a + b * x.col(1).col(1);
   std::cout << "max x2 is " << max(x2) << "\n";
   return 1;
   Type nll = -sum(dnorm(Y, mu, exp(logSigma), true));
