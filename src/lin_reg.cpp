8b04b703612d85579a4f753b5c557220f9cbd25e
Grant Adams <adamsgd@uw.edu>
2018-04-25 16:58:25 -0700

    trying to mess with matrix equation

--- src/lin_reg.cpp
+++ src/lin_reg.cpp
@@ -1,52 +1,43 @@
 #include <TMB.hpp>
 // Function for getting max of an Ivector
 template <class Type>
-Type max2(const vector<Type> &x)
-{
-  int res = x[0];
-  for(int i=0; i < x.size(); i++){
-    int res2 = x[i];
-    if(res < res2){
-      res = res2;
-      }
-  }
-  return res;
-}
+matrix<Type> pow_mat(matrix<Type> m1, Type exponent){
 
-template <class Type>
-Type mean_vec(const vector<Type> &x)
-{
-  Type mean = x.sum()/x.size();
-  return mean;
+  int m1r = m1.rows();
+  int m1c = m1.cols();
+
+  matrix<Type> m2(m1r, m1c);
+
+  // Elementwise division
+  for(int r = 0; r < m1r; r++){
+    for(int c = 0; c < m1c; c++){
+      m2(r, c) = pow( m1(r, c), exponent);
+    }
+  }
+  return m2;
 }
 
 template<class Type>
   Type objective_function<Type>::operator() (){
   DATA_VECTOR(Y);
   DATA_IVECTOR(ints);
-  DATA_ARRAY(x);
+  DATA_MATRIX(x);
   PARAMETER_VECTOR(a);
   PARAMETER(logSigma);
   ADREPORT(exp(2*logSigma));
 
   int n_data = Y.size();
   vector<Type> mu(n_data);
+  matrix<Type> m2()
   vector<Type> x2(n_data);
 
-  x2.setZero();
-  x2 += x.col(0).col(0);
+  x2 = x.col(0);
 
   mu = a(0) + x2 * a(1);
 
-  see(0) = x2;
-  see(1) = n_data;
-
-  std::cout << "max x2 is " << mean_vec(ints) << "\n";
-  // return 1;
   Type nll = -sum(dnorm(Y, mu, exp(logSigma), true));
 
   std::cout << "The nll is " << nll << "\n";
   ADREPORT(nll);
-  REPORT(see);
   return nll;
 }
