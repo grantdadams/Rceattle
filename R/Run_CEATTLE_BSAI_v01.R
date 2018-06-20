239bdaca65e730babd251560140143ca6efda3cb
Grant Adams <adamsgd@uw.edu>
2018-06-20 12:33:44 -0700

    Debugged CPP file. COMPILES AND BUILDS!
    went through line by line to debug the cpp file, everything compiles and builds without crashing.

--- R/Run_CEATTLE_BSAI_v01.R
+++ R/Run_CEATTLE_BSAI_v01.R
@@ -1,29 +1,29 @@
 # This functions runs CEATTLE
 
-Rceattle <- function( ctlFilename ){
+Rceattle <- function( ctlFilename = "asmnt2017_0", TMBfilename = "CEATTLE_BSAI_v01", dat_dir =  "data/dat files/" ){
   #--------------------------------------------------
   # 1. DATA and MODEL PREP
   #--------------------------------------------------
   # Check if require packages are installed and install if not
   if("TMB" %in% rownames(installed.packages()) == FALSE) {install.packages("TMB")}
-  # Load required packages
 
-
-  source("R/build_dat.R")
-  data_list <- build_dat(ctlFilename = "asmnt2017_0", TMBfilename = "CEATTLE_BSAI_v01", dat_dir = "data/dat files/")
-
+  # Load data
+  source("R/1-build_dat.R")
+  source("R/2-build_params.R")
+  data_list <- build_dat(ctlFilename, TMBfilename, dat_dir)
+  params <- build_params(data_list, nselages = 8)
 
 
   # Compile CEATTLE
-  library(TMBdebug)
-  setwd("tests")
-  version <- "data_load"
+  library(TMB)
+  setwd("src")
+  version <- "CEATTLE_BSAI_v01"
   compile(paste0(version, ".cpp"))
   dyn.load(dynlib(version))
   setwd("../")
 
   # Build object
-  Obj = TMBdebug::MakeADFun(data_list, parameters = list(),  DLL = version, type = "Fun")
+  Obj = TMB::MakeADFun(data_list, parameters = params,  DLL = version)
 
 
 }
