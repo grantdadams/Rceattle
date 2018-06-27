b2fbd94b8866c1a1955aaa9b22923da38b911f93
Grant Adams <adamsgd@uw.edu>
2018-06-26 22:54:30 -0700

    updated age_comp_check function

--- R/age_comp_check.R
+++ R/age_comp_check.R
@@ -1,24 +1,44 @@
+# Function to return relative error of survey age comps using data from TMB or ADMB
 rep <- Rceattle()
+species <- 1
 
-species <- 2
-NByage <- rep$NByage[,,species]
-F = rep$F[,,species]
-M1 = rep$M1[species,]
-srv_sel = rep$srv_sel[species,]
-ALK <- data_list$age_trans_matrix[,,species]
+age_comp_comparison <- function( species, rep, tmp, ADMB_TMB = 1){
 
-
-srv_age_hat <- sweep(NByage * exp(-0.5 * sweep(F, 2, M1, "+")), 2, srv_sel, "*")
-if(species == 1){
-  srv_age_com <- (srv_age_hat/ rowSums(srv_age_hat, na.rm = T))
-  srv_age_com <- srv_age_com[1:36, 1:12]
-}
-if(species > 1){
-  srv_len_hat <- matrix(NA, nrow = 36, ncol = 25)
-  for(i in 1:36){
-    srv_len_hat[i,] <- srv_age_hat[i,1:c(12,21)[species-1]] %*% ALK[1:c(12,21)[species-1], 1:25]
+  # TMB DATA
+  if(ADMB_TMB == 1){
+    NByage <- rep$NByage[,,species]
+    F = rep$F[,,species]
+    M1 = rep$M1[species,]
+    srv_sel = rep$srv_sel[species,]
+    ALK <- data_list$age_trans_matrix[,,species]
   }
-  srv_age_com <- (srv_len_hat/ rowSums(srv_len_hat, na.rm = T))
+
+  # ADMB DATA
+  if(ADMB_TMB == 2){
+    NByage <- tmp[[paste("NByage", species, sep = "_")]]
+    F = tmp[[paste("F", species, sep = "_")]]
+    M1 = tmp[[paste("M1", species, sep = "_")]]
+    srv_sel = tmp[[paste("srv_sel", species, sep = "_")]]
+    ALK <- data_list$age_trans_matrix[,,species]
+  }
+
+  srv_age_hat <- sweep(NByage, 2, srv_sel, "*") # sweep(NByage * exp(-0.5 * sweep(F, 2, M1, "+")), 2, srv_sel, "*")
+  if(species == 1){
+    srv_age_com <- (srv_age_hat/ rowSums(srv_age_hat, na.rm = T))
+    srv_age_com <- srv_age_com[1:36, 1:12]
+  }
+  if(species > 1){
+    srv_len_hat <- matrix(NA, nrow = 36, ncol = 25)
+    for(i in 1:36){
+      srv_len_hat[i,] <- srv_age_hat[i,1:c(12,21)[species-1]] %*% ALK[1:c(12,21)[species-1], 1:25]
+    }
+    srv_age_com <- (srv_len_hat/ rowSums(srv_len_hat, na.rm = T))
+  }
+
+  # srv_age_com == tmp[[paste("srv_age_hat", species, sep = "_")]]
+  rel_error <- (srv_age_com - tmp[[paste("srv_age_hat", species, sep = "_")]] )/ tmp[[paste("srv_age_hat", species, sep = "_")]]
+  return(rel_error)
 }
 
-srv_age_com == rep$srv_age_hat[1:36,,2]
+results_tmb <- age_comp_comparison( species = 1, rep, tmp, ADMB_TMB = 1)
+results_admb <- age_comp_comparison( species = 1, rep, tmp, ADMB_TMB = 2)
