69455e3283a254a564d1f85e15619ceb415cb233
Grant Adams <adamsgd@uw.edu>
2018-10-26 10:57:32 -0700

    included recruitment deviations as random effects
    Gets stuck on building?

--- R/tests/compare_predation_equations.R
+++ R/tests/compare_predation_equations.R
@@ -1,113 +1,133 @@
-# Year 1
-TMB = TRUE
+# Compares predation functions using TMB or ADMB average age estimates and using TMB based diet and ration calculations
+compare_pred_function <- function(rep, tmp, TMB = TRUE){
 
-if(TMB == TRUE){
-  AvgN <- list()
-  AvgN[[1]] <- rep$AvgN[,,1]
-  AvgN[[2]] <- rep$AvgN[,,2]
-  AvgN[[3]] <- rep$AvgN[,,3]
-}
-if(TMB == FALSE){
-  AvgN <- list()
-  AvgN[[1]] <- tmp$AvgN_1
-  AvgN[[2]] <- tmp$AvgN_2
-  AvgN[[3]] <- tmp$AvgN_3
-}
+  if(TMB == TRUE){
+    AvgN <- list()
+    AvgN[[1]] <- rep$AvgN[,,1]
+    AvgN[[2]] <- rep$AvgN[,,2]
+    AvgN[[3]] <- rep$AvgN[,,3]
 
-
-nyrs <- data_list$nyrs
-Uobs <- data_list$UobsAge
-
-of_stomkir <- matrix(NA, 21, 3)
-for(pred in 1:3){
-  for(pred_age in 1:21){
-    of_stomkir[pred_age, pred] <- 1 - sum( rep$stomKir[pred,,pred_age,,1], na.rm = T)
+    ration <- list()
+    ration[[1]] <- rep$ration2Age[,,1]
+    ration[[2]] <- rep$ration2Age[,,2]
+    ration[[3]] <- rep$ration2Age[,,3]
   }
-}
-of_stomkir <- of_stomkir/data_list$other_food[1]
+  if(TMB == FALSE){
+    AvgN <- list()
+    AvgN[[1]] <- tmp$AvgN_1
+    AvgN[[2]] <- tmp$AvgN_2
+    AvgN[[3]] <- tmp$AvgN_3
 
-# of_stomKir food should be good
+    ration <- list()
+    ration[[1]] <- tmp$ration2_1
+    ration[[2]] <- tmp$ration2_2
+    ration[[3]] <- tmp$ration2_3
+  }
 
-# Stomach proportion
-stom_div_bio2 <- array(NA, dim = c(3,3,21,21, nyrs))
-suma_suit <- array(0, dim = c(nyrs, 21, 3))
-for(yr in 1:nyrs){
-  for(prey in 1:3){
-    for(pred in 1:3){
-      for(prey_age in 1:data_list$nages[prey]){
-        for(pred_age in 1:data_list$nages[pred]){
-          stom_div_bio2[pred,prey,pred_age,prey_age, yr] <- rep$stomKir[pred,prey,pred_age,prey_age,1] / (AvgN[[prey]][yr, prey_age] * data_list$wt[yr, prey_age, prey])
-          suma_suit[yr,pred_age,pred] <- suma_suit[yr,pred_age,pred] + stom_div_bio2[pred,prey,pred_age,prey_age,yr]
-        }
-      }
+
+  nyrs <- data_list$nyrs
+  Uobs <- data_list$UobsAge
+
+  of_stomkir <- matrix(NA, 21, 3)
+  for(pred in 1:3){
+    for(pred_age in 1:21){
+      of_stomkir[pred_age, pred] <- 1 - sum( rep$stomKir[pred,,pred_age,,1], na.rm = T)
     }
   }
-}
-suma_suit[1,,1]
+  of_stomkir <- of_stomkir/data_list$other_food[1]
 
-# Suitability
-suit_main <- array(0, dim = c(3,3,21,21))
-suit_other <- matrix(0, 21, 3)
-for(pred in 1:3){
-  for(pred_age in 1:data_list$nages[pred]){
-    suit_other[pred_age, pred] = 1
+  # of_stomKir food should be good
+
+  # Stomach proportion
+  stom_div_bio2 <- array(NA, dim = c(3,3,21,21, nyrs))
+  suma_suit <- array(0, dim = c(nyrs, 21, 3))
+  for(yr in 1:nyrs){
     for(prey in 1:3){
-      for(prey_age in 1:data_list$nages[prey]){
-
-        for(yr in 1:nyrs){
-          suit_main[pred,prey,pred_age,prey_age] <- suit_main[pred,prey,pred_age,prey_age] + stom_div_bio2[pred,prey,pred_age,prey_age, yr] / (suma_suit[yr, pred_age, pred] + of_stomkir[pred_age,pred])
+      for(pred in 1:3){
+        for(prey_age in 1:data_list$nages[prey]){
+          for(pred_age in 1:data_list$nages[pred]){
+            stom_div_bio2[pred,prey,pred_age,prey_age, yr] <- rep$stomKir[pred,prey,pred_age,prey_age,1] / (AvgN[[prey]][yr, prey_age] * data_list$wt[yr, prey_age, prey])
+            suma_suit[yr,pred_age,pred] <- suma_suit[yr,pred_age,pred] + stom_div_bio2[pred,prey,pred_age,prey_age,yr]
+          }
         }
-        suit_main[pred,prey,pred_age,prey_age] = suit_main[pred,prey,pred_age,prey_age] / nyrs
-        suit_other[pred_age, pred] <- suit_other[pred_age, pred] - suit_main[pred,prey,pred_age,prey_age]
       }
     }
   }
-}
+  suma_suit[1,,1]
 
-suit_other
-
-# Compare
-tmp$suit_other_1
-tmp$suit_other_2
-tmp$suit_other_3
-
-# Available food
-avail_food <- array(0, dim = c(nyrs, 21, 3))
-for(pred in 1:3){
-  for(pred_age in 1:data_list$nages[pred]){
-    for(yr in 1:nyrs){
-      tmp_othersuit = 0
+  # Suitability
+  suit_main <- array(0, dim = c(3,3,21,21))
+  suit_other <- matrix(0, 21, 3)
+  for(pred in 1:3){
+    for(pred_age in 1:data_list$nages[pred]){
+      suit_other[pred_age, pred] = 1
       for(prey in 1:3){
         for(prey_age in 1:data_list$nages[prey]){
 
-          avail_food[yr, pred_age, pred]  = avail_food[yr, pred_age, pred] + suit_main[pred,prey,pred_age,prey_age] * AvgN[[prey]][yr,prey_age] * data_list$wt[yr, prey_age, prey]
-          tmp_othersuit = tmp_othersuit + suit_main[pred,prey,pred_age,prey_age]
-        }
-      }
-      avail_food[yr, pred_age, pred] = avail_food[yr, pred_age, pred]  + data_list$other_food[1]*(1 - (tmp_othersuit))
-    }
-  }
-}
-
-tmp$avail_food_1[1,]
-tmp$avail_food_2[1,]
-tmp$avail_food_3[1,]
-
-# Calculate M2
-M2 <- array(0, dim = c(nyrs, 21, 3))
-B_eaten <- array(0, dim = c(nyrs, 21, 3))
-for(prey in 1:3){
-  for(prey_age in 1:data_list$nages[prey]){
-    for(yr in 1:nyrs){
-      for(pred in 1:3){
-        for(pred_age in 1:data_list$nages[pred]){
-          M2[yr, prey_age, prey] = M2[yr, prey_age, prey] + ((AvgN[[pred]][yr,pred_age] * rep$ration2Age[yr, pred_age, pred]*suit_main[pred,prey,pred_age,prey_age])/avail_food[yr, pred_age, pred])
-          B_eaten[yr, prey_age, prey] = B_eaten[yr, prey_age, prey] + (AvgN[[pred]][yr,pred_age] * rep$ration2Age[yr, pred_age, pred]*suit_main[pred,prey,pred_age,prey_age])
+          for(yr in 1:nyrs){
+            suit_main[pred,prey,pred_age,prey_age] <- suit_main[pred,prey,pred_age,prey_age] + stom_div_bio2[pred,prey,pred_age,prey_age, yr] / (suma_suit[yr, pred_age, pred] + of_stomkir[pred_age,pred])
+          }
+          suit_main[pred,prey,pred_age,prey_age] = suit_main[pred,prey,pred_age,prey_age] / nyrs
+          suit_other[pred_age, pred] <- suit_other[pred_age, pred] - suit_main[pred,prey,pred_age,prey_age]
         }
       }
     }
   }
+
+  suit_other
+
+  # Compare
+  tmp$suit_other_1
+  tmp$suit_other_2
+  tmp$suit_other_3
+
+  # Available food
+  avail_food <- array(0, dim = c(nyrs, 21, 3))
+  for(pred in 1:3){
+    for(pred_age in 1:data_list$nages[pred]){
+      for(yr in 1:nyrs){
+        tmp_othersuit = 0
+        for(prey in 1:3){
+          for(prey_age in 1:data_list$nages[prey]){
+
+            avail_food[yr, pred_age, pred]  = avail_food[yr, pred_age, pred] + suit_main[pred,prey,pred_age,prey_age] * AvgN[[prey]][yr,prey_age] * data_list$wt[yr, prey_age, prey]
+            tmp_othersuit = tmp_othersuit + suit_main[pred,prey,pred_age,prey_age]
+          }
+        }
+        avail_food[yr, pred_age, pred] = avail_food[yr, pred_age, pred]  + data_list$other_food[1]*(1 - (tmp_othersuit))
+      }
+    }
+  }
+
+  tmp$avail_food_1[1,]
+  tmp$avail_food_2[1,]
+  tmp$avail_food_3[1,]
+
+  # Calculate M2
+  M2 <- array(0, dim = c(nyrs, 21, 3))
+  B_eaten <- array(0, dim = c(nyrs, 21, 3))
+  for(prey in 1:3){
+    for(prey_age in 1:data_list$nages[prey]){
+      for(yr in 1:nyrs){
+        for(pred in 1:3){
+          for(pred_age in 1:data_list$nages[pred]){
+            M2[yr, prey_age, prey] = M2[yr, prey_age, prey] + ((AvgN[[pred]][yr,pred_age] * ration[[pred]][yr,pred_age] * suit_main[pred,prey,pred_age,prey_age])/avail_food[yr, pred_age, pred])
+            B_eaten[yr, prey_age, prey] = B_eaten[yr, prey_age, prey] + (AvgN[[pred]][yr,pred_age] * ration[[pred]][yr,pred_age] * suit_main[pred,prey,pred_age,prey_age])
+          }
+        }
+      }
+    }
+  }
+
+  results <- list(R_M2 = M2, TMB_M2 = rep$M2)
+  return(results)
 }
-sum((M2[1,1:12,1] - tmp$M2_1) > 1e-10)
-M2[1,1:12,1]
-rep$M2[1,1:12,1]
+
+admb_est <- compare_pred_function(ms_rep, ms_tmp, TMB = FALSE)
+rounder = 4
+# Species 1
+admb_est$R_M2[,1:12,1]
+ms_tmp$M2_1
+round(ms_tmp$M2_1, rounder) == round(admb_est$R_M2[,1:12,1], rounder)
+
+round(ms_rep$ration2Age[,1:12,1],4) == round(ms_tmp$ration2_1,4)
--- Report/October_2018_tests.R
+++ Report/October_2018_tests.R
@@ -1,7 +1,7 @@
 ###################################################
 # Run in single species mode
 load("data/BS_SS_Files/2017_assessment_data_list.RData")
-ss_run <- Rceattle(data_list = data_list_ss, ctlFilename = "asmnt2017_0A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_SS_Files/dat files/", debug = TRUE, inits = "ceattle.par")
+ss_run <- Rceattle(data_list = data_list_ss, ctlFilename = "asmnt2017_0A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_SS_Files/dat files/", debug = TRUE, inits = "ceattle.par", plot_trajectory = FALSE, random_rec = FALSE)
 ss_rep <- ss_run$rep
 
 # Load previous estimates
@@ -41,6 +41,78 @@ ms_tmp$obj_fun
 
 ms_rep$M2[1,,1]
 ms_tmp$M2_1[1,]
+
+# CHECK 1 -  Check N[1,1,nspp] - CLEAR
+exp(ms_run$params$ln_mn_rec + ms_run$params$rec_dev[,1])
+ms_rep$NByage[1,1,]
+c(ms_tmp$NByage_1[1,1], ms_tmp$NByage_2[1,1], ms_tmp$NByage_3[1,1])
+
+# CHECK 2 - Check N0 - CLEAR
+# Species 1
+exp(ms_run$params$ln_mn_rec[1] + ms_run$params$rec_dev[1,])
+ms_rep$NByage[,1,1]
+ms_tmp$NByage_1[,1]
+
+# Species 2
+exp(ms_run$params$ln_mn_rec[2] + ms_run$params$rec_dev[2,])
+ms_rep$NByage[,1,2]
+ms_tmp$NByage_2[,1]
+
+# Species 3
+exp(ms_run$params$ln_mn_rec[3] + ms_run$params$rec_dev[3,])
+ms_rep$NByage[,1,3]
+ms_tmp$NByage_3[,1]
+
+# CHECK 3 - Check F
+# Species 1
+exp(ms_run$params$ln_mean_F[1] + ms_run$params$F_dev[1,])
+ms_rep$F[,1,1]
+ms_tmp$F_1[,1]
+
+# Species 2
+ms_rep$F[,1,2]
+ms_tmp$F_2[,1]
+
+# Species 3
+ms_rep$F[,1,3]
+ms_tmp$F_3[,1]
+
+# CHECK 4 - Compare M2
+# TMB avgN inputs
+tmb_est <- compare_pred_function(ms_rep, ms_tmp, TMB = TRUE)
+# Species 1
+tmb_est$R_M2[,1:12,1]
+tmb_est$TMB_M2[,1:12,1]
+round(tmb_est$TMB_M2[,1:12,1],6) == round(tmb_est$R_M2[,1:12,1],6)
+
+# Species 2
+tmb_est$R_M2[,1:21,2]
+tmb_est$TMB_M2[,1:21,2]
+round(tmb_est$TMB_M2[,1:21,2],6) == round(tmb_est$R_M2[,1:21,2],6)
+
+# Species 3
+tmb_est$R_M2[,1:21,3]
+tmb_est$TMB_M2[,1:21,3]
+round(tmb_est$TMB_M2[,1:21,3],6) == round(tmb_est$R_M2[,1:21,3],6)
+
+
+# ADMB avgN inputs
+admb_est <- compare_pred_function(ms_rep, ms_tmp, TMB = FALSE)
+rounder = 2
+# Species 1
+admb_est$R_M2[,1:12,1]
+ms_tmp$M2_1
+round(ms_tmp$M2_1, rounder) == round(admb_est$R_M2[,1:12,1], rounder)
+
+# Species 2
+admb_est$R_M2[,1:21,2]
+ms_tmp$M2_2
+round(ms_tmp$M2_2[,1:12], rounder) == round(admb_est$R_M2[,1:12,2], rounder)
+
+# Species 3
+admb_est$R_M2[,1:21,3]
+ms_tmp$M2_3
+round(ms_tmp$M2_3, rounder) == round(admb_est$R_M2[,1:21,3], rounder)
 
 ###################################################
 # Run in MS mode using std files
