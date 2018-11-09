b521c61b45f5537e53608903c87b614e1b8b08cb
Grant Adams <adamsgd@uw.edu>
2018-11-09 09:54:33 -0800

    fixed age_trans_matrix conversion for srv age comp

--- Report/October_2018_tests.R
+++ Report/October_2018_tests.R
@@ -3,28 +3,32 @@
 version <- "CEATTLE_BSAI_MS_v01"
 
 load("data/BS_SS_Files/2017_assessment_data_list.RData")
-ss_run <- Rceattle(data_list = data_list_ss, ctlFilename = "asmnt2017_0A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_SS_Files/dat files/", inits = "ceattle.par", debug = TRUE, plot_trajectory = FALSE, random_rec = FALSE, niter = 3)
+ss_run <- Rceattle(data_list = data_list_ss, ctlFilename = "asmnt2017_0A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_SS_Files/dat files/", inits = NULL, debug = FALSE, plot_trajectory = FALSE, random_rec = FALSE, niter = 3)
 ss_rep <- ss_run$rep
 
 # Load previous estimates
 load("data/BS_SS_Files/CEATTLE_results.Rdata")
 ss_tmp <- tmp
+tmb_like <- sum(ss_rep$jnll_comp)
 
 # Compare with current
-ss_res <- compare_output(ss_rep, ss_tmp, rel_error = 0.00001)
+ss_res <- compare_output(ss_rep, ss_tmp, rel_error = 0.0001)
 ss_res$summary
 ss_res$likelihood
-ss_res$tmb_like
-ss_res$admb_like
+tmb_like_comp <- ss_res$tmb_like
+admb_like_cop <- ss_res$admb_like
 
-sum(ss_rep$jnll_comp)
+tmb_like_comp
+admb_like_cop
+tmb_like
+admb_like
 ss_tmp$obj_fun
 
 
 ###################################################
 # Run in MS mode using par files
 load("data/BS_MS_Files/2017_assessment_data_list.RData")
-ms_run <- Rceattle(data_list = data_list_ms, ctlFilename = "asmnt2017_2A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_MS_Files/dat files/", inits = "ceattle.par", debug = TRUE, plot_trajectory = FALSE, random_rec = FALSE, niter = 3)
+ms_run <- Rceattle(data_list = data_list_ms, ctlFilename = "asmnt2017_2A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01", dat_dir =  "data/BS_MS_Files/dat files/", inits = "ceattle.par", debug = FALSE, plot_trajectory = FALSE, random_rec = FALSE, niter = 3)
 ms_rep <- ms_run$rep
 
 # Load previous estimates
