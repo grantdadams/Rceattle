7025eaaa913d9c440de9a51637528fc08d43a2dc
Grant Adams <adamsgd@uw.edu>
2019-01-02 12:39:12 -0800

    updated indexing for cpp file

---
+++ R/tests/kinzey_output_test.R
@@ -0,0 +1,31 @@
+data_list_ss <- build_dat(ctlFilename = "asmnt2017_0A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01_02", dat_dir = "data/BSAI/BS_SS_Files/dat files/", nspp = 3)
+
+ss_run <- Rceattle(TMBfilename = "CEATTLE_BSAI_MS_v01_02",
+                   data_list = data_list_ss,
+                   inits = NULL, # Initial parameters = 0
+                   file_name = NULL, # Don't save
+                   debug = 0, # Estimate
+                   random_rec = FALSE, # No random recruitment
+                   msmMode = 0, # Single species mode
+                   avgnMode = 0,
+                   silent = FALSE)
+
+
+
+data_list_ms <- build_dat(ctlFilename = "asmnt2017_2A_corrected", TMBfilename = "CEATTLE_BSAI_MS_v01_02", dat_dir = "data/BSAI/BS_MS_Files/dat files/", nspp = 3)
+
+ms_run <- Rceattle(TMBfilename = "CEATTLE_BSAI_MS_v01_02",
+                   data_list = data_list_ms,
+                   inits = ss_run$estimated_params, # Initial parameters from ss run
+                   file_name = NULL, # Don't save
+                   debug = 0, # Estimate
+                   random_rec = FALSE, # No random recruitment
+                   niter = 10, # Number of iterations around predation/pop dy functions
+                   msmMode = 1, # Multi-species holsman mode
+                   avgnMode = 0)
+
+plot_biomass(ceattle_list =  list(ms_run, ss_run), model_names = c("MS", "SS"))
+plot_recruitment(ceattle_list =  list(ms_run, ss_run), model_names = c("MS", "SS"))
+
+
+ms_run$quantities$NByage[,,1]
