ce7bdd7aab886953fbe9352a3562baadb63bf5ae
Grant Adams <adamsgd@uw.edu>
2019-03-22 13:05:24 -0700

    updated index test

--- R/tests/index_test.R
+++ R/tests/index_test.R
@@ -2,41 +2,43 @@
 library(TMB)
 library(TMBhelper)
 library(TMBdebug)
+library(Rceattle)
 
 # Load data
 # source("R/2-build_params.R")
 # source("R/3-build_map.R")
 data_list_ss$nages
-TMBfilename = "ceattle_v01_02"
+cpp_directory <- system.file("executables",package="Rceattle")
+TMBfilename <- "ceattle_v01_02"
 data("BS2017SS")
 data_list = BS2017SS
 inits = NULL # Initial parameters = 0
 file_name = NULL # Don't save
-debug = 1 # Estimate
+debug = 0 # Estimate
 random_rec = FALSE # No random recruitment
-msmMode = 1 # Single species mode
+msmMode = 0 # Single species mode
 avgnMode = 0
 silent = FALSE
 niter = 10
 est_diet = FALSE
 suitMode = FALSE
+map <- NULL
+bounds <- NULL
+
+setwd(getwd())
 
 #--------------------------------------------------
 # 1. DATA and MODEL PREP
 #--------------------------------------------------
 # # Check if require packages are installed and install if not
 # if ("TMB" %in% rownames(installed.packages()) == FALSE) {
-#   install.packages("TMB")
+#  install.packages("TMB")
 # }
 # if ("TMBhelper" %in% rownames(installed.packages()) == FALSE) {
-#   install.packages("TMBhelper")
+#  install.packages("TMBhelper")
 # }
 # library(TMB)
 # library(TMBhelper)
-#
-# # Load data
-# # source("R/2-build_params.R")
-# # source("R/3-build_map.R")
 
 
 # STEP 1 - LOAD DATA
@@ -51,18 +53,27 @@ data_list$debug <- debug
 data_list$niter <- niter
 data_list$avgnMode <- avgnMode
 data_list$msmMode <- msmMode
-data_list$suitMode <- suitMode
-data_list$est_diet <- est_diet
+data_list$suitMode <- as.numeric(suitMode)
+
+
+# Get cpp file if not provided
+if(is.null(TMBfilename) | is.null(cpp_directory)){
+  cpp_directory <- system.file("executables",package="Rceattle")
+  TMBfilename <- "ceattle_v01_02"
+} else{
+  cpp_directory <- cpp_directory
+  TMBfilename <- TMBfilename
+}
 
 
 # STEP 1 - LOAD PARAMETERS
 if (is.character(inits) | is.null(inits)) {
-  params <- Rceattle::build_params(
+  params <- suppressWarnings(Rceattle::build_params(
     data_list = data_list,
-    nselages = 8,
     inits = inits,
-    TMBfilename = TMBfilename
-  )
+    TMBfilename = TMBfilename,
+    cpp_directory = cpp_directory
+  ))
 } else{
   params <- inits
 }
@@ -71,14 +82,22 @@ print("Step 1: Parameter build complete")
 
 
 # STEP 2 - BUILD MAP
-map  <-
-  Rceattle::build_map(data_list, params, debug = debug, random_rec = random_rec)
+if (is.null(map)) {
+  map <-
+    suppressWarnings(Rceattle::build_map(data_list, params, debug = debug, random_rec = random_rec))
+} else{
+  map <- map
+}
 print("Step 2: Map build complete")
 
 
 # STEP 3 - Get bounds
-bounds <- Rceattle::build_bounds(param_list = params)
-
+if (is.null(bounds)) {
+  bounds <- Rceattle::build_bounds(param_list = params, data_list)
+} else {
+  bounds = bounds
+}
+print("Step 3: Param bounds complete")
 
 
 # STEP 4 - Setup random effects
@@ -87,30 +106,41 @@ if (random_rec == TRUE) {
   random_vars <- c("rec_dev")
 }
 
+'%!in%' <- function(x,y)!('%in%'(x,y))
 
-# STEP 5 - Compile CEATTLE
-version <- TMBfilename
-cpp_directory <- "inst"
-cpp_file <- paste0(cpp_directory, "/", version)
+# STEP 5 - Compile CEATTLE is providing cpp file
+cpp_file <- paste0(cpp_directory, "/", TMBfilename)
 
 # Remove compiled files if not compatible with system
 version_files <-
-  list.files(path = cpp_directory, pattern = version)
+  list.files(path = cpp_directory, pattern = TMBfilename)
 if (Sys.info()[1] == "Windows" &
-    paste0(version, ".so") %in% version_files) {
+    paste0(TMBfilename, ".so") %in% version_files) {
   suppressWarnings(try(dyn.unload(TMB::dynlib(paste0(cpp_file)))))
-  file.remove(paste0(cpp_file, ".so"))
-  file.remove(paste0(cpp_file, ".o"))
+  suppressWarnings(file.remove(paste0(cpp_file, ".so")))
+  suppressWarnings(file.remove(paste0(cpp_file, ".o")))
 }
 if (Sys.info()[1] != "Windows" &
-    paste0(version, ".dll") %in% version_files) {
+    paste0(TMBfilename, ".dll") %in% version_files) {
   suppressWarnings(try(dyn.unload(TMB::dynlib(paste0(cpp_file)))))
-  file.remove(paste0(cpp_file, ".dll"))
-  file.remove(paste0(cpp_file, ".o"))
+  suppressWarnings(file.remove(paste0(cpp_file, ".dll")))
+  suppressWarnings(file.remove(paste0(cpp_file, ".o")))
+}
+if (Sys.info()[1] != "Windows" &
+    paste0(TMBfilename, ".so") %!in% version_files) {
+  suppressWarnings(try(dyn.unload(TMB::dynlib(paste0(cpp_file)))))
+  suppressWarnings(file.remove(paste0(cpp_file, ".dll")))
+  suppressWarnings(file.remove(paste0(cpp_file, ".o")))
+}
+if(recompile){
+  suppressWarnings(try(dyn.unload(TMB::dynlib(paste0(cpp_file)))))
+  suppressWarnings(file.remove(paste0(cpp_file, ".dll")))
+  suppressWarnings(file.remove(paste0(cpp_file, ".so")))
+  suppressWarnings(file.remove(paste0(cpp_file, ".o")))
 }
 
 TMB::compile(paste0(cpp_file, ".cpp"))
-dyn.load(TMB::dynlib(paste0(cpp_file)))
+dyn.load(TMB::dynlib(paste0(cpp_file)), silent = TRUE)
 print("Step 4: Compile CEATTLE complete")
 
 
@@ -119,7 +149,7 @@ print("Step 4: Compile CEATTLE complete")
 obj = TMB::MakeADFun(
   data_list,
   parameters = params,
-  DLL = "Rceattle",
+  DLL = TMBfilename,
   map = map,
   random = random_vars,
   silent = silent
@@ -127,49 +157,24 @@ obj = TMB::MakeADFun(
 )
 
 
-print(paste0("Step 5: Optimizing model"), hessian = TRUE)
-# opt <- nlminb(obj$par, obj$fn, obj$gr)
-# methods <- c('Nelder-Mead', 'BFGS', 'CG', 'L-BFGS-B', 'nlm', 'nlminb', 'spg', 'ucminf', 'newuoa', 'bobyqa', 'nmkb', 'hjkb', 'Rcgmin', 'Rvmmin')
-# opt_list <- list()
-# for(i in 1:length(methods)){
-#   opt_list[i] = optimx(obj$par, function(x) as.numeric(obj$fn(x)), obj$gr, control = list(maxit = 10000), method = methods[i])
-# }
-opt = tryCatch(
-  TMBhelper::Optimize(obj),
-  lower = bounds$lower,
-  upper = bounds$upper,
-  error = function(e)
-    NULL,
-  loopnum = 3
+
+# Remove inactive parameters from bounds and vectorize
+L = unlist(bounds$lower)[which(!is.na(unlist(map)))]
+U = unlist(bounds$upper)[which(!is.na(unlist(map)))]
+
+# Remove random effects from bounds
+if (random_rec == TRUE) {
+  L <- L[-grep(random_vars, names(L))]
+  U <- U[-grep(random_vars, names(U))]
+}
+
+# Optimize
+opt = Rceattle::Optimize(obj = obj,
+                         fn=obj$fn,
+                         gr=obj$gr,
+                         startpar=obj$par,
+                         lower = L,
+                         upper = U,
+                         loopnum = 5
 )
-
-# Get quantities
-sdrep = TMB::sdreport(obj)
-quantities <- obj$report(obj$env$last.par.best)
-
-if (debug) {
-  last_par <- params
-}
-else if (random_rec == F) {
-  last_par = suppressWarnings(obj$env$parList(obj$env$last.par.best))
-}
-else{
-  last_par = suppressWarnings(obj$env$parList(obj$env$last.par.best))
-}
-
-run_time = ((Sys.time() - start_time))
-
-# Return objects
-mod_objects <-
-  list(
-    data_list = data_list,
-    initial_params = params,
-    estimated_params = last_par,
-    map = map,
-    sdrep = sdrep,
-    obj = obj,
-    opt = opt,
-    quantities = quantities,
-    bounds = bounds,
-    run_time = run_time
-  )
+Check_Identifiable(obj)
