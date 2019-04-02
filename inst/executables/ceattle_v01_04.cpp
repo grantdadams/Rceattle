943934b42d8e45ac283982b21bc269d67d78152a
Grant Adams <adamsgd@uw.edu>
2019-04-02 11:59:47 -0700

    updating cpp to be better

--- inst/executables/ceattle_v01_04.cpp
+++ inst/executables/ceattle_v01_04.cpp
@@ -46,7 +46,219 @@
 // ------------------------------------------------------------------------- //
 // 0. LOAD DEPENDENCIES                                                      //
 // ------------------------------------------------------------------------- //
-#include "functions.hpp"
+
+// Function for getting max of an IVECTOR and return an int
+template <class Type>
+Type imax(const vector<Type> &x)
+{
+  int res = x[0];
+  for(int i=0; i < x.size(); i++){
+    int res2 = x[i];
+    if(res < res2){
+      res = res2;
+    }
+  }
+  return res;
+}
+
+// Function for detecting NAs
+template<class Type>
+bool isNA(Type x){
+  return R_IsNA(asDouble(x));
+}
+
+//  Function to getting sqaure
+template <class Type> Type square(Type x){return x*x;}
+
+// Function for getting min of an IVECTOR and return an int
+template <class Type>
+Type imin(const vector<Type> &x)
+{
+  int res = x[0];
+  for(int i=0; i < x.size(); i++){
+    int res2 = x[i];
+    if(res > res2){
+      res = res2;
+    }
+  }
+  return res;
+}
+
+// Function for mean of a vector
+template <class Type>
+Type mean(const vector<Type> &x)
+{
+  Type mean = x.sum()/x.size();
+  return mean;
+}
+
+// Function to calculate the first difference
+template <class Type>
+vector<Type> first_difference(const vector<Type> &x)
+{
+  int length = x.size() - 1;
+
+  if (length <= 0){
+    std::cerr << "Error -- vector size too small in first_difference(const dvector&)" << std::endl;
+    return(0);
+  }
+
+  vector<Type> tmp(length);
+
+  for (int i = 0; i < length; i++){
+    tmp(i) = x(i+1) - x(i);
+  }
+
+  return tmp;
+}
+
+// Function for elementwise division
+template <class Type>
+matrix<Type> elem_div(matrix<Type> m1, matrix<Type> m2){
+
+  int m1r = m1.rows();
+  int m2r = m2.rows();
+
+  int m1c = m1.cols();
+  int m2c = m1.cols();
+
+  if (m1r != m2r){
+    std::cerr << "Error -- number of rows in matrices does not match" << std::endl;
+    return(0);
+  }
+
+  if (m1c != m2c){
+    std::cerr << "Error -- number of columns in matrices does not match" << std::endl;
+    return(0);
+  }
+
+  matrix<Type> m3(m1r, m1c);
+
+  // Elementwise division
+  for(int r = 0; r < m1r; r++){
+    for(int c = 0; c < m1c; c++){
+      m3(r, c) = m1(r, c) / m2(r, c);
+    }
+  }
+  return m3;
+}
+
+// Function for elementwise matrix exponential functions
+template <class Type>
+matrix<Type> elem_pow(matrix<Type> m1, Type exponent){
+
+  int nrow = m1.rows();
+  int ncol = m1.cols();
+
+  matrix<Type> m2(nrow, ncol);
+
+  // Elementwise division
+  for(int r = 0; r < nrow; r++){
+    for(int c = 0; c < ncol; c++){
+      m2(r, c) = pow( m1(r, c), exponent);
+    }
+  }
+  return m2;
+}
+
+// Function for to extract a layer from an array
+template<class Type>
+matrix<Type> matrix_from_array(array<Type> a1, int sheet){
+  vector<int> a1_dim = a1.dim;
+  matrix<Type> m1(a1_dim(0), a1_dim(1));
+
+  // If array is array
+  if(a1_dim.size() == 3){
+    for(int row = 0; row < a1_dim(0); row++){
+      for(int col = 0; col < a1_dim(1); col++){
+        m1(row, col) = a1(row, col, sheet);
+      }
+    }
+  }
+
+  // If array is matrix
+  if(a1_dim.size() == 2){
+    for(int row = 0; row < a1_dim(0); row++){
+      for(int col = 0; col < a1_dim(1); col++){
+        m1(row, col) = a1(row, col);
+      }
+    }
+  }
+  return m1;
+}
+
+// Function for the multiplaction of a vector and matrix: in r "v %*% m"
+template <class Type>
+vector<Type> vec_mat_prod(vector<Type> v1, matrix<Type> m1){
+
+  if (v1.size() != m1.rows()){
+    std::cerr << "Error -- length of vector does not equal the number of rows in the matrix" << std::endl;
+    return(0);
+  }
+
+  vector<Type> v2(m1.rows());
+  vector<Type> v3(m1.cols());
+
+  // Vector * matrix
+  for(int c = 0; c < m1.cols(); c++){
+    v2 = m1.col(c);
+    v3(c) = (v1 * v2).sum();
+  }
+  return v3;
+}
+
+
+// function to get row vector from array
+template<class Type>
+vector<Type> col_from_3D_array(array<Type> a1, int col, int sheet){
+  vector<int> a1_dim = a1.dim;
+  vector<Type> v1(a1_dim(0));
+
+  // If array is array
+  if(a1_dim.size() == 3){
+    for(int row = 0; row < a1_dim(0); row++){
+      v1(row) = a1(row, col, sheet);
+    }
+  }
+
+  // If array is matrix
+  if(a1_dim.size() == 2){
+    for(int row = 0; row < a1_dim(0); row++){
+      v1(row) = a1(row, col);
+    }
+  }
+  return v1;
+}
+
+
+// Function to trim matrix
+template<class Type>
+matrix<Type> trim_matrix(matrix<Type> m1, int nrow, int ncol){
+  // dim is a DATA_IMATRIX with structure nrow, ncol, ID # of object we wish to interogate
+  matrix<Type> m2(nrow, ncol);
+
+  // If array is matrix
+  for(int row = 0; row < nrow; row++){
+    for(int col = 0; col < ncol; col++){
+      m2(row, col) = m1(row, col);
+    }
+  }
+  return m2;
+}
+
+// Function to trim vector
+template<class Type>
+matrix<Type> trim_vector(vector<Type> v1, int length){
+  // dim is a DATA_IMATRIX with structure nrow, ncol, ID # of object we wish to interogate
+  vector<Type> v2(length);
+
+  // If array is matrix
+  for(int i = 0; i < length; i++){
+    v2(i) = v1(i);
+  }
+  return v2;
+}
+
 
 template<class Type>
 Type objective_function<Type>::operator() () {
@@ -157,7 +369,7 @@ Type objective_function<Type>::operator() () {
   DATA_ARRAY( UobsWtAge );                // pred, prey, predA, preyA U matrix (mean wt_hat of prey in each pred age); n = [nspp, nspp, max_age, max_age]
   DATA_MATRIX( Mn_LatAge );               // Mean length-at-age; n = [nspp, nages], ALSO: mean_laa in Kinzey
 
-  // 2.3.8. Environmental data              
+  // 2.3.8. Environmental data
   DATA_IVECTOR( Tyrs );                   // Years of hindcast data; n = [1, nTyrs] #FIXME - changed the name of this in retro_data2017_asssmnt.dat
   DATA_VECTOR( BTempC );                  // Vector of bottom temperature; n = [1,  nTyrs ]
   int nTyrs = Tyrs.size();                // Number of temperature years; n = [1] #FIXME - changed the name of this in retro_data2017_asssmnt.dat
@@ -763,7 +975,7 @@ Type objective_function<Type>::operator() () {
             ConsumAge(sp, age, yr) = CA(sp) * pow(wt(yr, age, pop_wt_index(sp)) * Type(1000), CB( sp )) // C_max = CA * W ^ CB; where C_max is grams consumed per grams of predator per day
             * fT(sp, yr) * fday( sp ) * wt(yr, age, pop_wt_index(sp)) * 1000; //  C_max * f(T) * wt * fday g/pred.yr
             ConsumAge(sp, age, yr) = ConsumAge(sp, age, yr) * Pvalue(sp) * Pyrs(yr, age, sp); //
-          
+
 
           ration2Age(sp, age, yr) = ConsumAge(sp, age, yr) / 1000;      // Annual ration kg/yr //aLW(predd)*pow(lengths(predd,age),bLW(predd));//mnwt_bin(predd,age);
         }
