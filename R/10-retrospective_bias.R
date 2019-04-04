08f203740b6635ab92b77817e21ec7c684500a29
Grant Adams <adamsgd@uw.edu>
2019-04-04 10:03:52 -0700

    changing bias to remove models that did not converge

--- R/10-retrospective_bias.R
+++ R/10-retrospective_bias.R
@@ -13,7 +13,7 @@
 #'
 #' ss_run <- Rceattle::fit_mod(data_list = BS2017SS,
 #'                             inits = NULL, # Initial parameters = 0
-#'                             file_name = NULL, # Don't save
+#'                             file = NULL, # Don't save
 #'                             debug = 0, # Estimate
 #'                             random_rec = FALSE, # No random recruitment
 #'                             msmMode = 0, # Single species mode
@@ -33,6 +33,7 @@ retrospective <- function( Rceattle = NULL, peels = NULL){
   styr <- data_list$styr
 
   # Run across retrospective bits
+  ind <- 2
   for(i in 1:peels){
     data_list$endyr <- endyr - i
     nyrs <- (endyr - i) - styr + 1
@@ -42,18 +43,24 @@ retrospective <- function( Rceattle = NULL, peels = NULL){
     inits$rec_dev <- inits$rec_dev[,1:nyrs]
     inits$F_dev <- inits$F_dev[,1:nyrs]
 
-    # Refit model
-    mod_list[[i + 1]] <- Rceattle::fit_mod(
+    # Refit
+    newmod <- suppressMessages(suppressWarnings( Rceattle::fit_mod(
       data_list = data_list,
       inits = inits,
-      file_name = NULL,
+      file = NULL,
       debug = data_list$debug,
       niter = data_list$niter,
       random_rec = data_list$random_rec,
       msmMode = data_list$msmMode,
       suitMode = data_list$suitMode,
       avgnMode = data_list$avgnMode,
-      silent = TRUE)
+      silent = TRUE) ))
+
+    # Refit model
+    if(newmod$opt$Convergence_check != "The model is definitely not converged" | !is.null(newmod$opt$Convergence_check)){
+      mod_list[[ind]] <- newmod
+      ind <- ind + 1
+    }
   }
 
   # Calculate Mohs rho for each species
@@ -63,10 +70,9 @@ retrospective <- function( Rceattle = NULL, peels = NULL){
   colnames(mohns) <- c("Object", paste0("Species_", 1:data_list$nspp))
   mohns$Object <- objects
 
-  # Loop around peels
-  for(i in 1:peels){
-    data_list$endyr <- endyr - i
-    nyrs_peel <- (endyr - i) - styr + 1
+  # Loop around peels that converged
+  for(i in 1:length(mod_list)){
+    nyrs_peel <- mod_list[[i]]$data_list$endyr - styr + 1
 
     # Loop around derived quantitities
     for(j in 1:length(objects)){
@@ -102,5 +108,5 @@ retrospective <- function( Rceattle = NULL, peels = NULL){
   #   }
   # }
 
-  return(list(Rceattle_list = mod_list, mohns = mohns))
+  return(list(Rceattle_list = rev(mod_list), mohns = mohns))
 }
