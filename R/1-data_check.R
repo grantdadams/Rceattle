f556de7dcbead45ab274b7f7db8eabdda2b35853
Grant Adams <adamsgd@uw.edu>
2019-05-21 10:35:50 -0700

    update m1 check

--- R/1-data_check.R
+++ R/1-data_check.R
@@ -5,6 +5,10 @@ data_check <- function(data_list){
     stop("Length of temperature series is not the same as the number of temperature years")
   }
 
+  if(length(data_list$M1_base) == 1){
+    stop("M1 is a single value, please make it age/species specific")
+  }
+
   if(sum(data_list$other_food < 0) > 0){
     stop("Other food for one species is negative")
   }
