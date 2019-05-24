7aa265bf21b7061c0ffdcc71b6ff7816168f9495
Grant Adams <adamsgd@uw.edu>
2019-05-24 12:14:37 -0700

    made code tidy

--- R/1-data_check.R
+++ R/1-data_check.R
@@ -1,15 +1,14 @@
-data_check <- function(data_list){
+data_check <- function(data_list) {
 
+    if (length(data_list$BTempC) != length(data_list$Tyrs)) {
+        stop("Length of temperature series is not the same as the number of temperature years")
+    }
 
-  if(length(data_list$BTempC) != length(data_list$Tyrs)){
-    stop("Length of temperature series is not the same as the number of temperature years")
-  }
+    if (length(data_list$M1_base) == 1) {
+        stop("M1 is a single value, please make it age/species specific")
+    }
 
-  if(length(data_list$M1_base) == 1){
-    stop("M1 is a single value, please make it age/species specific")
-  }
-
-  if(sum(data_list$other_food < 0) > 0){
-    stop("Other food for one species is negative")
-  }
+    if (sum(data_list$other_food < 0) > 0) {
+        stop("Other food for one species is negative")
+    }
 }
