66c866a9423692f4a522b3e4ebe61d512db624ab
Grant Adams <adamsgd@uw.edu>
2019-09-16 11:05:07 -0700

    Update Rceattle_install_run_BSAI.R

--- vignettes/Rceattle_install_run_BSAI.R
+++ vignettes/Rceattle_install_run_BSAI.R
@@ -30,8 +30,6 @@ data(BS2017SS) # ?BS2017SS for more information on the data
 # # Change the data how you want in excel
 # # Read the data back in
 # mydata <- Rceattle::read_data( file = "BS2017SS.xlsx")
-
-
 phaseList = list(
       dummy = 1,
       ln_mn_rec = 1,
@@ -84,71 +82,6 @@ ss_run <- Rceattle::fit_mod(data_list = BS2017SS,
                             silent = TRUE,
                             recompile = FALSE)
 
-# Type ?fit_mod for more details
-ests <- mod_objects$estimated_params
-inits <- ss_run$estimated_params
-inits$ln_mn_rec <- ests$ln_mn_rec
-
-inits$ln_mean_F[1:3] <- ests$ln_mean_F
-
-inits$log_srv_q[7] <- ests$log_srv_q[4]
-inits$sel_inf[,4:7,1] <- ests$srv_sel_inf
-inits$sel_slp[,4:7,1] <- ests$srv_sel_slp
-inits$sel_coff[1:3,] <- ests$fsh_sel_coff
-inits$init_dev <- ests$init_dev
-inits$F_dev[1:3,] <- ests$F_dev
-inits$rec_dev <- ests$rec_dev
-
-
-ss_run2 <- Rceattle::fit_mod(data_list = BS2017SS,
-                            TMBfilename = "ceattle_v01_06",
-                            cpp_directory = "inst/executables",
-                            inits = NULL, # Initial parameters = 0
-                            file = NULL, # Don't save
-                            debug = 0, # Estimate
-                            random_rec = FALSE, # No random recruitment
-                            msmMode = 0, # Single species mode
-                            silent = TRUE,
-                            recompile = FALSE,
-                            niter = 10)
-
-
-round(ss_run$quantities$jnll_comp, 3)
-round(mod_objects$quantities$jnll_comp, 3)
-
-sum(ss_run$quantities$jnll_comp)
-sum(mod_objects$quantities$jnll_comp)
-
-
-ss_run2$quantities$srv_bio_hat
-mod_objects$quantities$srv_bio_hat
-
-
-ss_run2$quantities$S[1,1,2,]
-mod_objects$quantities$S[1,2,]
-
-ss_run2$quantities$NByage[1,1,,10:13]
-mod_objects$quantities$NByage[1,,10:13]
-
-ss_run2$quantities$Zed[1,1,,10:13]
-mod_objects$quantities$Zed[1,,10:13]
-
-ss_run2$quantities$sel[7,1,,10:13]
-mod_objects$quantities$srv_sel[4,,10:13]
-
-ss_run2$quantities$sel[1,1,,10:13]
-mod_objects$quantities$fsh_sel[1,,10:13]
-
-
-
-
-ss_run2$quantities$flt_spp
-ss_run2$quantities$flt_type
-ss_run2$quantities$flt_sel_type
-
-
-mod_objects$quantities$jnll_comp
-
 # The you can plot the model results using using
 plot_biomass(Rceattle =  ss_run)
 plot_recruitment(Rceattle =  ss_run)
@@ -156,25 +89,11 @@ plot_recruitment(Rceattle =  ss_run)
 
 # For the a multispecies model starting from the single species parameters, the following can be specified to load the data:
 data("BS2017MS") # Note: the only difference is the residual mortality (M1_base) is lower
-# Or we can use the previous data set
-ests <- mod_objects$estimated_params
-inits <- ss_run$estimated_params
-inits$ln_mn_rec <- ests$ln_mn_rec
-
-inits$ln_mean_F[1:3] <- ests$ln_mean_F
-
-inits$log_srv_q[7] <- ests$log_srv_q[4]
-inits$sel_inf[,4:7,1] <- ests$srv_sel_inf
-inits$sel_slp[,4:7,1] <- ests$srv_sel_slp
-inits$sel_coff[1:3,] <- ests$fsh_sel_coff
-inits$init_dev <- ests$init_dev
-inits$F_dev[1:3,] <- ests$F_dev
-inits$rec_dev <- ests$rec_dev
 
 ms_run <- Rceattle::fit_mod(data_list = BS2017MS,
                             TMBfilename = "ceattle_v01_06",
                             cpp_directory = "inst/executables",
-                            inits = inits, # Initial parameters from single species ests
+                            inits = ss_run$estimated_params, # Initial parameters from single species ests
                             file = NULL, # Don't save
                             debug = 0, # Estimate
                             niter = 10, # 10 iterations around population and predation dynamics
@@ -184,12 +103,10 @@ ms_run <- Rceattle::fit_mod(data_list = BS2017MS,
                             silent = TRUE,
                             recompile = FALSE)
 
-sum(ms_run$quantities$jnll_comp)
-sum(mod_objects$quantities$jnll_comp)
 
 # We can plot both runs as well:
-mod_list <- list(ss_run, mod_objects)
-mod_names <- c("SS", "SS-old")
+mod_list <- list(ss_run, ms_run)
+mod_names <- c("SS", "MS")
 
 # Plot biomass trajectory
 plot_biomass(Rceattle = mod_list, model_names = mod_names)
