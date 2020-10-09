42704b1df10bca4107e60ce629aa94f0529dd822
Grant Adams <adamsgd@uw.edu>
2020-10-09 10:03:06 -0700

    Added residual plots for CPUE series

--- R/7-plot_diagnostics.R
+++ R/7-plot_diagnostics.R
@@ -85,8 +85,8 @@ plot_index <- function(Rceattle,
   for(srv in 1:nsrv){
     for(i in 1:length(Rceattle)){
       srv_ind <- which(Srv_list[[i]]$Fleet_code == srvs[srv])
-      ymax[srv] <- max((c(Srv_list[[i]]$Upper95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind])), na.rm = T)
-      ymin[srv] <- min((c(Srv_list[[i]]$Lower95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind])), na.rm = T)
+      ymax[srv] <- max(c(Srv_list[[i]]$Upper95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind], ymax[srv]), na.rm = T)
+      ymin[srv] <- min(c(Srv_list[[i]]$Lower95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind], ymin[srv]), na.rm = T)
     }
   }
   ymax <- ymax + top_adj * (ymax-ymin)
@@ -315,8 +315,8 @@ plot_catch <- function(Rceattle,
   for(fsh in 1:nfsh){
     for(i in 1:length(Rceattle)){
       fsh_ind <- which(fsh_list[[i]]$Fleet_code == fshs[fsh])
-      ymax[fsh] <- max((c(fsh_list[[i]]$Upper95[fsh_ind], fsh_hat_list[[i]]$Catch[fsh_ind])), na.rm = T)
-      ymin[fsh] <- min((c(fsh_list[[i]]$Lower95[fsh_ind], fsh_hat_list[[i]]$Catch[fsh_ind])), na.rm = T)
+      ymax[fsh] <- max(c(fsh_list[[i]]$Upper95[fsh_ind], fsh_hat_list[[i]]$Catch[fsh_ind], ymax[fsh]), na.rm = T)
+      ymin[fsh] <- min(c(fsh_list[[i]]$Lower95[fsh_ind], fsh_hat_list[[i]]$Catch[fsh_ind], ymin[fsh]), na.rm = T)
     }
   }
   ymax <- ymax + top_adj * (ymax-ymin)
@@ -540,8 +540,8 @@ plot_logindex <- function(Rceattle,
   for(srv in 1:nsrv){
     for(i in 1:length(Rceattle)){
       srv_ind <- which(Srv_list[[i]]$Fleet_code == srvs[srv])
-      ymax[srv] <- max(log(c(Srv_list[[i]]$Upper95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind])), na.rm = T)
-      ymin[srv] <- min(log(c(Srv_list[[i]]$Lower95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind])), na.rm = T)
+      ymax[srv] <- max(log(c(Srv_list[[i]]$Upper95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind], ymax[srv])), na.rm = T)
+      ymin[srv] <- min(log(c(Srv_list[[i]]$Lower95[srv_ind], Srv_hat_list[[i]]$Observation[srv_ind], ymin[srv])), na.rm = T)
     }
   }
   ymax <- ymax + top_adj * (ymax-ymin)
@@ -677,3 +677,227 @@ plot_logindex <- function(Rceattle,
     }
   }
 } # End of logfit
+
+
+
+#' CPUE residual
+#'
+#' Plot of residuals CPUE indices on log-scale (r4ss-style)
+#'
+#' @param file name of a file to identified the files exported by the
+#'   function.
+#' @param Rceattle Single or list of Rceattle model objects exported from \code{\link{Rceattle}}
+#' @param single.plots if TRUE plot invidual fits else make multiplot
+#' @param model_names Names of models to be used in legend
+#' @param line_col Colors of models to be used for line color
+#' @param species Species names for legend
+#' @param right_adj How much right side of the x-axis for fitting the legend. As percentage.
+#' @param top_adj How much top side of the y-axis for fitting the legend. As percentage.
+#' @param incl_proj TRUE/FALSE include projections years
+#' @param width plot width
+#' @param height plot hight
+#' @export
+
+plot_indexresidual <- function(Rceattle,
+                               file = NULL,
+                               model_names = NULL,
+                               line_col = NULL,
+                               species = NULL,
+                               right_adj = 0,
+                               top_adj = 0.05,
+                               incl_proj = FALSE,
+                               single.plots=FALSE,
+                               width=NULL,
+                               height=NULL){
+
+
+  # Convert single one into a list
+  if(class(Rceattle) == "Rceattle"){
+    Rceattle <- list(Rceattle)
+  }
+
+  # Species names
+  if(is.null(species)){
+    species =  Rceattle[[1]]$data_list$spnames
+  }
+
+
+  # Extract data objects
+  Years <- list()
+  Endyrs <- list()
+  Srv_hat_list <- list()
+  for(i in 1:length(Rceattle)){
+    Endyrs[[i]] <- Rceattle[[i]]$data_list$endyr
+    if(incl_proj == FALSE){
+      Years[[i]] <- Rceattle[[i]]$data_list$styr:Rceattle[[i]]$data_list$endyr
+    }
+    if(incl_proj){
+      Years[[i]] <- Rceattle[[i]]$data_list$styr:Rceattle[[i]]$data_list$projyr
+    }
+
+    # Get estimate and residual
+    Srv_hat_list[[i]] <- Rceattle[[i]]$data_list$srv_biom
+    Srv_hat_list[[i]]$Estimate <- Rceattle[[i]]$quantities$srv_bio_hat
+    Srv_hat_list[[i]]$Residual <- log(Rceattle[[i]]$quantities$srv_bio_hat) - log(Rceattle[[i]]$data_list$srv_biom$Observation)
+  }
+  max_endyr <- max(unlist(Endyrs), na.rm = TRUE)
+  nyrs_vec <- sapply(Years, length)
+  nyrs <- max(nyrs_vec)
+
+  nspp <- Rceattle[[1]]$data_list$nspp
+
+
+  # Plot limits
+  fleet_control <- (Rceattle[[1]]$data_list$fleet_control)
+  srv_biom <- (Rceattle[[1]]$data_list$srv_biom)
+  srvs <- sort(unique(srv_biom$Fleet_code))
+  nsrv <- length(srvs)
+
+  ymax <- c()
+  ymin <- c()
+
+  for(srv in 1:nsrv){
+    for(i in 1:length(Rceattle)){
+      srv_ind <- which(Srv_hat_list[[i]]$Fleet_code == srvs[srv])
+      ymax[srv] <- max((c(ymax[srv], Srv_hat_list[[i]]$Residual[srv_ind])), na.rm = T)
+      ymin[srv] <- min((c(ymin[srv], Srv_hat_list[[i]]$Residual[srv_ind])), na.rm = T)
+    }
+  }
+  ymax <- ymax + top_adj * (ymax-ymin)
+
+  positions=seq(-0.2, 0.2, length.out = length(Rceattle))
+
+  # Assume colors if not provided
+  if (is.null(line_col)) {
+    line_col <- rev(oce::oce.colorsViridis(length(Rceattle)))
+  }
+
+  # Plot trajectory
+  loops <- ifelse(is.null(file), 1, 2)
+  for (j in 1:loops) {
+
+    # Plot/save each survey individually
+    if(single.plots==TRUE){
+      if(is.null(width)) width = 5
+      if(is.null(height)) height = 3.5
+      for(srv in 1:nsrv){
+        Par = list(mfrow=c(1,1),mar = c(3.5, 3.5, 0.5, 0.1), mgp =c(2.,0.5,0), tck = -0.02,cex=0.8)
+
+        # Save
+        if(j == 2){
+          filename <- paste0(file, "fleet",srvs[j]," ",as.character(fleet_control$Fleet_name[srvs[srv]]), "_survey_index", ".png")
+          png(file = filename, width = width, height = height, res = 200, units = "in")
+        }
+
+        minyr <- min(sapply(Srv_hat_list, function(x) min(x[which(x$Fleet_code == srvs[srv]),]$Year)))
+        maxyr <- max(sapply(Srv_hat_list, function(x) max(x[which(x$Fleet_code == srvs[srv]),]$Year)))
+
+        par(Par)
+        plot(NA, NA, ylab="Index residual", xlab="Year", ylim = c((ymin[srv]), (ymax[srv])), xlim = c(minyr, maxyr + (maxyr - minyr) * right_adj), type='n', xaxt="n", yaxt="n")
+        abline(h = 0, lty = 2)
+
+        axis(1,labels=TRUE,cex=0.8)
+        axis(2,labels=TRUE,cex=0.8)
+
+        # Loop through models
+        for (k in 1:length(Rceattle)) {
+
+          # Subset data by fleet and model
+          srv_tmp <- Srv_hat_list[[k]][which(Srv_hat_list[[k]]$Fleet_code == srvs[srv]),]
+          srv_hat_tmp <- Srv_hat_list[[k]][which(Srv_hat_list[[k]]$Fleet_code == srvs[srv]),]
+
+          # Plot residual
+          for(yr in 1:length(srv_hat_tmp$Year)){
+            lines(rep((srv_hat_tmp$Year[yr] + positions[k]),2),c(0, srv_hat_tmp$Residual[yr]), col = line_col[k])
+          }
+          points(srv_hat_tmp$Year + positions[k], srv_hat_tmp$Residual, col=1, pch=21, bg = line_col[k])
+        }
+
+        # Index name
+        legend('topleft',as.character(fleet_control$Fleet_name[srvs[srv]]),bty="n",y.intersp = -0.2,cex=0.8)
+
+        # Model names
+        if(!is.null(model_names)){
+          legend(
+            "topright",
+            legend = model_names,
+            pch = rep(16, length(line_col)), cex=0.8,
+            col = line_col,
+            bty = "n"
+          )
+
+        }
+        # Save plot
+        if(j == 2){dev.off()}
+      }
+    }
+
+    # Plot/save each survey individually
+    if(single.plots==FALSE){
+
+      # Set heights of plot
+      if(is.null(width)) width = 7
+      if(is.null(height)) height = ifelse(nsrv==1,5,ifelse(nsrv==2,3.,2.5))*round(nsrv/2+0.01,0)
+
+
+      Par = list(mfrow=c(round(nsrv/2+0.01,0),ifelse(nsrv==1,1,2)),mai=c(0.35,0.15,0,.15),omi = c(0.2,0.25,0.2,0) + 0.1,mgp=c(2,0.5,0), tck = -0.02,cex=0.8)
+
+      # Save
+      if(j == 2){
+        filename <- paste0(file,"_survey_indices", ".png")
+        png(file = filename, width = width, height = height, res = 200, units = "in")
+      }
+      par(Par)
+
+
+      for(srv in 1:nsrv){
+        minyr <- min(sapply(Srv_hat_list, function(x) min(x[which(x$Fleet_code == srvs[srv]),]$Year)))
+        maxyr <- max(sapply(Srv_hat_list, function(x) max(x[which(x$Fleet_code == srvs[srv]),]$Year)))
+
+        xlim <- c(minyr, maxyr)
+        if(srv == 1){
+          xlim <- c(minyr, maxyr + (maxyr - minyr) * right_adj)
+        }
+
+        plot(NA, NA, ylab="", xlab="", ylim = c((ymin[srv]), (ymax[srv])), xlim = xlim, type='n', xaxt="n", yaxt="n")
+        abline(h = 0, lty = 2)
+        axis(1,labels=TRUE,cex=0.8)
+        axis(2,labels=TRUE,cex=0.8)
+
+        # Index name
+        legend('topleft',as.character(fleet_control$Fleet_name[srvs[srv]]),bty="n",y.intersp = -0.2,cex=0.8)
+
+        # Model names
+        if(srv == 1){
+          if(!is.null(model_names)){
+            legend(
+              "topright",
+              legend = model_names,
+              pch = rep(16, length(line_col)), cex=0.8,
+              col = line_col,
+              bty = "n"
+            )
+          }
+        }
+
+        # Loop through models
+        for (k in 1:length(Rceattle)) {
+
+          # Subset data by fleet and model
+          srv_tmp <- Srv_hat_list[[k]][which(Srv_hat_list[[k]]$Fleet_code == srvs[srv]),]
+          srv_hat_tmp <- Srv_hat_list[[k]][which(Srv_hat_list[[k]]$Fleet_code == srvs[srv]),]
+
+          # Plot residual
+          for(yr in 1:length(srv_hat_tmp$Year)){
+            lines(rep((srv_hat_tmp$Year[yr] + positions[k]),2),c(0, srv_hat_tmp$Residual[yr]), col = line_col[k])
+          }
+          points(srv_hat_tmp$Year + positions[k], srv_hat_tmp$Residual, col=1, pch=21, bg = line_col[k])
+        }
+      }
+    }
+    mtext(paste("Year"), side=1, outer=TRUE, at=0.5,line=1,cex=1)
+    mtext(paste("Index residual"), side=2, outer=TRUE, at=0.5,line=1,cex=1)
+    if(j == 2){dev.off()}
+  }
+} # End of plot
+
