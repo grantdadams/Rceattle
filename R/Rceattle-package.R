0cde4cc73262feb810a294f5fbf3907f00e48cec
grantdadams <grantadams60091@gmail.com>
2021-06-13 10:41:21 -0700

    cleaned up and made package version streamslined

---
+++ R/Rceattle-package.R
@@ -0,0 +1,9 @@
+#' @rawNamespace useDynLib(Rceattle, .registration=TRUE); useDynLib(Rceattle2_TMBExports)
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
--- R/tests/n_at_age_comparison.R
+++
@@ -1,9 +0,0 @@
-load("data/BS_MS_3_Loops_Files/CEATTLE_results.Rdata")
-pollock_suitability_avgN <- tmp$AvgNsuit_1
-pollock_M2_avgN <- tmp$AvgNM2_1
-
-colnames(pollock_M2_avgN) <- paste0("Age_", 1:ncol(pollock_M2_avgN))
-colnames(pollock_suitability_avgN) <- paste0("Age_", 1:ncol(pollock_suitability_avgN))
-
-pollock_suitability_avgN[1:10,]
-pollock_M2_avgN[1:10,]
