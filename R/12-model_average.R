383963b2c37437bddb30537d148dc07e4b9b841c
Grant Adams <grantadams60091@gmail.com>
2021-08-26 16:50:16 -0700

    Uncertainty in model averaging, minor plot edits
    - model_average now calculates uncertainty for SSB, B, and R via weighted bootstrap from mles and joint precision matrix of models assuming asymtptotic normal. May take some time to do for random effects models
    - updated plot functions to have width and height and model legend cex and the y axis adjustment to be flexible
    - Changes getHessian to getJointPrecision in fit_mod

--- R/12-model_average.R
+++ R/12-model_average.R
@@ -2,12 +2,16 @@
 #'
 #' @param Rceattle list of Rceattle model objects
 #' @param weights vector of weights to be used for weighting models
+#' @param Uncertainty TRUE/FALSE Sample uncertainty across derived quantities using weighted bootstrap from the asymptotic  distribution of MLEs
+#' @param nboot Number of bootstraps taken from asymptotic distribution of MLEs. Default = 10000
 #'
 #' @return an Rceattle object with derived quantities weighted by the specified weights. The length of the derived quantities spans the years which overlap across all models.
 #' @export
 #'
-model_average <- function(Rceattle, weights = NULL){
-
+model_average <- function(Rceattle, weights = NULL, uncertainty = FALSE, nboot = 10000){
+  # --------------------------------------------------------------------------------------------
+  # Average derived quantities of models
+  # --------------------------------------------------------------------------------------------
   # Convert single one into a list
   if(class(Rceattle) == "Rceattle"){
     stop("Only one model provided")
@@ -16,6 +20,11 @@ model_average <- function(Rceattle, weights = NULL){
     weights = rep(1/length(Rceattle), length(Rceattle))
   }
   weights <- weights/sum(weights)
+
+  # Extract number of species of each model
+  nspp <- sapply(Rceattle, function(x) x$data_list$nspp)
+  nspp <- unique(nspp)
+  if(length(nspp) > 1){stop("Number of species does not match across models")}
 
   # Extract timespan of each model
   years <- sapply(Rceattle, function(x) x$data_list$styr:x$data_list$projyr)
@@ -32,7 +41,8 @@ model_average <- function(Rceattle, weights = NULL){
   mod_avg <- Rceattle[[which(nyrs == min(nyrs))[1]]] # Copy a model of the smallest length to fill in
   mod_avg_rel_proj_yrs <- (max_styr - mod_avg$data_list$styr + 1) : (min_projyr - mod_avg$data_list$styr + 1) # Find years of overlap
   mod_avg_rel_hind_yrs <- (max_styr - mod_avg$data_list$styr + 1) : (min_hindyr - mod_avg$data_list$styr + 1) # Find years of overlap
-  mod_avg$estimated_params <- mod_avg$initial_params <- mod_avg$opt <- mod_avg$run_time <- mod_avg$sdrep <- mod_avg$obj <- mod_avg$map <- mod_avg$bounds <- NULL
+  mod_avg$estimated_params <- mod_avg$initial_params <- mod_avg$opt <- mod_avg$run_time <- mod_avg$obj <- mod_avg$map <- mod_avg$bounds <- NULL
+  if(!uncertainty){mod_avg$sdrep <- NULL}
 
   # -- Set all quantities to zero
   for(i in 1:length(mod_avg$quantities)){
@@ -119,6 +129,269 @@ model_average <- function(Rceattle, weights = NULL){
     mod_avg$quantities$B_eaten_prop[,,,,mod_avg_rel_proj_yrs] <- mod_avg$quantities$B_eaten_prop[,,,,mod_avg_rel_proj_yrs] + Rceattle[[i]]$quantities$B_eaten_prop[,,,,sub_rel_proj_yrs] * weights[i]
     # mod_avg$quantities$UobsAge_hat );
     # mod_avg$quantities$UobsWtAge_hat );
+  }
+
+  # --------------------------------------------------------------------------------------------
+  # Sample uncertainty across derived quantities weighted bootstrap from the asymptotic intervals
+  # --------------------------------------------------------------------------------------------
+
+  if(uncertainty){
+
+    # # Buckland, S.T., Burnham, K.P., Augustin, N.H., 1997. Model Selection : An Integral Part of Inference. Biometrics 53, 603–618.
+    # # - Calculate SD
+    # # -- R
+    # rec_rows <- which(names(mod_avg$sdrep$value) == "R")
+    # mod_avg$sdrep$sd[rec_rows] <- 0
+    # mod_avg$sdrep$value[rec_rows] <- 0
+    #
+    #
+    # # -- B
+    # biomass_rows <- which(names(mod_avg$sdrep$value) == "biomass")
+    # mod_avg$sdrep$sd[biomass_rows] <- 0
+    # mod_avg$sdrep$value[biomass_rows] <- 0
+    #
+    #
+    # # -- SSB
+    # ssb_rows <- which(names(mod_avg$sdrep$value) == "biomassSSB")
+    # mod_avg$sdrep$sd[ssb_rows] <- 0
+    # mod_avg$sdrep$value[ssb_rows] <- 0
+    #
+    # # Loop across models and calculate mean
+    # for(i in 1:length(Rceattle)){
+    #   # Years
+    #   sub_rel_proj_yrs <- (max_styr - Rceattle[[i]]$data_list$styr + 1) : (min_projyr - Rceattle[[i]]$data_list$styr + 1) # Find years of overlap
+    #
+    #
+    #   # Extract Rec, SSB, B
+    #   # - R
+    #   mu_rec <- array(NA, dim = c(nspp, nyrs[i]))
+    #   rec_rows <- which(names(Rceattle[[i]]$sdrep$value) == "R")
+    #   mu_rec <- replace(mu_rec, values = Rceattle[[i]]$sdrep$value[rec_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   # - B
+    #   mu_biomass <- array(NA, dim = c(nspp, nyrs[i]))
+    #   biomass_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomass")
+    #   mu_biomass <- replace(mu_biomass, values = Rceattle[[i]]$sdrep$value[biomass_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   # - SSB
+    #   mu_biomassSSB <- array(NA, dim = c(nspp, nyrs[i]))
+    #   ssb_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomassSSB")
+    #   mu_biomassSSB <- replace(mu_biomassSSB, values = Rceattle[[i]]$sdrep$value[ssb_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #
+    #   # - Calculate weighted mean
+    #   # -- R
+    #   rec_rows <- which(names(mod_avg$sdrep$value) == "R")
+    #   mod_avg$sdrep$value[rec_rows] <- mod_avg$sdrep$value[rec_rows] + c(mu_rec * weights[i])
+    #
+    #
+    #   # -- B
+    #   biomass_rows <- which(names(mod_avg$sdrep$value) == "biomass")
+    #   mod_avg$sdrep$value[biomass_rows] <- mod_avg$sdrep$value[biomass_rows] + c(mu_biomass * weights[i])
+    #
+    #
+    #   # -- SSB
+    #   ssb_rows <- which(names(mod_avg$sdrep$value) == "biomassSSB")
+    #   mod_avg$sdrep$value[ssb_rows] <- mod_avg$sdrep$value[ssb_rows] + c(mu_biomassSSB * weights[i])
+    # }
+    #
+    #
+    # # Loop across models and calculate var
+    # for(i in 1:length(Rceattle)){
+    #   # - R
+    #   sd_rec <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_rec <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   rec_rows <- which(names(Rceattle[[i]]$sdrep$value) == "R")
+    #   sd_rec <- replace(sd_rec, values = Rceattle[[i]]$sdrep$sd[rec_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_rec <- replace(mu_rec, values = Rceattle[[i]]$sdrep$value[rec_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   # - B
+    #   sd_biomass <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_biomass <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   biomass_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomass")
+    #   sd_biomass <- replace(sd_biomass, values = Rceattle[[i]]$sdrep$sd[biomass_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_biomass <- replace(mu_biomass, values = Rceattle[[i]]$sdrep$value[biomass_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   # - SSB
+    #   sd_biomassSSB <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_biomassSSB <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   ssb_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomassSSB")
+    #   sd_biomassSSB <- replace(sd_biomassSSB, values = Rceattle[[i]]$sdrep$sd[ssb_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_biomassSSB <- replace(mu_biomassSSB, values = Rceattle[[i]]$sdrep$value[ssb_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #
+    #   # - Calculate weighted mean
+    #   # -- R
+    #   rec_rows <- which(names(mod_avg$sdrep$value) == "R")
+    #   mod_avg$sdrep$sd[rec_rows] <- mod_avg$sdrep$sd[rec_rows] + weights[i] * sqrt(sd_rec^2 + (mu_rec - mod_avg$sdrep$value[rec_rows])^2)
+    #
+    #
+    #   # -- B
+    #   biomass_rows <- which(names(mod_avg$sdrep$value) == "biomass")
+    #   mod_avg$sdrep$sd[biomass_rows] <- mod_avg$sdrep$sd[biomass_rows] + weights[i] * sqrt(sd_biomass^2 + (mu_biomass - mod_avg$sdrep$value[biomass_rows])^2)
+    #
+    #
+    #   # -- SSB
+    #   ssb_rows <- which(names(mod_avg$sdrep$value) == "biomassSSB")
+    #   mod_avg$sdrep$sd[ssb_rows] <- mod_avg$sdrep$sd[ssb_rows] + weights[i] * sqrt(sd_biomassSSB^2 + (mu_biomassSSB - mod_avg$sdrep$value[ssb_rows])^2)
+    # }
+    #
+
+
+
+    # Using univariate normal asymptotics of ssb
+    # weights <- round(weights * nboot)
+    # nrows <- 1:sum(weights)
+    # rowid <- rep(1:length(weights), weights)
+    #
+    # # List to save samples of SSB, B, and R
+    # samples_rec <- list()
+    # samples_biomassSSB <- list()
+    # samples_biomass <- list()
+    #
+    # # Loop across models
+    # for(i in 1:length(Rceattle)){
+    #   # Years
+    #   sub_rel_proj_yrs <- (max_styr - Rceattle[[i]]$data_list$styr + 1) : (min_projyr - Rceattle[[i]]$data_list$styr + 1) # Find years of overlap
+    #
+    #
+    #   # Extract Rec, SSB, B
+    #   # - R
+    #   sd_rec <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_rec <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   rec_rows <- which(names(Rceattle[[i]]$sdrep$value) == "R")
+    #   sd_rec <- replace(sd_rec, values = Rceattle[[i]]$sdrep$sd[rec_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_rec <- replace(mu_rec, values = Rceattle[[i]]$sdrep$value[rec_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   samples_rec[[i]] <- MASS::mvrnorm(weights[i], mu = c(mu_rec), Sigma = diag(c(sd_rec)))
+    #
+    #
+    #   # - B
+    #   sd_biomass <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_biomass <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   biomass_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomass")
+    #   sd_biomass <- replace(sd_biomass, values = Rceattle[[i]]$sdrep$sd[biomass_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_biomass <- replace(mu_biomass, values = Rceattle[[i]]$sdrep$value[biomass_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   samples_biomass[[i]] <- MASS::mvrnorm(weights[i], mu = c(mu_biomass), Sigma = diag(c(sd_biomass)))
+    #
+    #
+    #   # - SSB
+    #   sd_biomassSSB <- array(NA, dim = c(nspp, nyrs[i]))
+    #   mu_biomassSSB <- array(NA, dim = c(nspp, nyrs[i]))
+    #
+    #   ssb_rows <- which(names(Rceattle[[i]]$sdrep$value) == "biomassSSB")
+    #   sd_biomassSSB <- replace(sd_biomassSSB, values = Rceattle[[i]]$sdrep$sd[ssb_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #   mu_biomassSSB <- replace(mu_biomassSSB, values = Rceattle[[i]]$sdrep$value[ssb_rows])[,sub_rel_proj_yrs] # Remove years not shared across models
+    #
+    #   samples_biomassSSB[[i]] <- MASS::mvrnorm(weights[i], mu = c(mu_biomassSSB), Sigma = diag(c(sd_biomassSSB)))
+    # }
+    #
+    #
+    # # - Save samples
+    # mod_avg$asymptotic_samples <- list(recruitment = do.call(rbind, samples_rec),
+    #                                    biomassSSB = do.call(rbind, samples_biomassSSB),
+    #                                    biomass = do.call(rbind, samples_biomass))
+    #
+    # # - Calculate SD
+    # # -- R
+    # rec_rows <- which(names(mod_avg$sdrep$value) == "R")
+    # mod_avg$sdrep$sd[rec_rows] <- sqrt(apply(mod_avg$asymptotic_samples$recruitment, 2, var))
+    # check <- data.frame(Sim =  mod_avg$sdrep$sd[rec_rows], Mod3 = Rceattle[[3]]$sdrep$sd[rec_rows])
+    # check$diff <- check$Sim - check$Mod3
+    # check
+    #
+    # mod_avg$sdrep$value[rec_rows] <- colMeans(mod_avg$asymptotic_samples$recruitment)
+    #
+    #
+    # # -- B
+    # biomass_rows <- which(names(mod_avg$sdrep$value) == "biomass")
+    # mod_avg$sdrep$sd[biomass_rows] <- sqrt(apply(mod_avg$asymptotic_samples$biomass, 2, var))
+    # mod_avg$sdrep$value[biomass_rows] <- colMeans(mod_avg$asymptotic_samples$biomass)
+    #
+    #
+    # # -- SSB
+    # ssb_rows <- which(names(mod_avg$sdrep$value) == "biomassSSB")
+    # mod_avg$sdrep$sd[ssb_rows] <- sqrt(apply(mod_avg$asymptotic_samples$biomassSSB, 2, var))
+    # mod_avg$sdrep$value[ssb_rows] <- colMeans(mod_avg$asymptotic_samples$biomassSSB)
+
+
+    # Assuming asymptotic multivariate normal
+    # - Must have estimated joint precision matrix
+    length_ran <- sapply(Rceattle, function(x) !is.null(x$sdrep$par.random)) # All parameters random effects
+    joint_est <- sapply(Rceattle, function(x) is.null(x$sdrep$jointPrecision))
+    if(sum(length_ran) > 0){
+      if(sum(joint_est) > 0){
+        stop(paste("Model(s):", which(joint_est > 0)," do not have joint precision matrices: re-estimate with getJointPrecision = TRUE"))
+      }
+
+      if(sum(length_ran) != length(Rceattle)){
+        stop("Does not currently support both purely fixed and random effects models")
+      }
+    }
+
+
+
+    # - Pointers
+    weights <- round(weights * nboot)
+    nrows <- 1:sum(weights)
+    rowid <- rep(1:length(weights), weights)
+
+    # List to save samples of SSB, B, and R
+    recruitment <-
+      array(NA, dim = c(nspp, length(mod_avg_rel_proj_yrs),  sum(weights)))
+    biomassSSB <-
+      array(NA, dim = c(nspp, length(mod_avg_rel_proj_yrs),  sum(weights)))
+    biomass <-
+      array(NA, dim = c(nspp, length(mod_avg_rel_proj_yrs),  sum(weights)))
+
+
+    # Loop across models
+    for(i in 1:length(Rceattle)){
+
+      # - Sample parameters from asymptotic normal distribution
+      # - Models with random effects
+      if(sum(length_ran) > 0){
+      mle <- Rceattle[[i]]$obj$env$last.par.best # Includes fixed and random
+      vcov <- solve(Rceattle[[i]]$sdrep$jointPrecision) # names(mle) == rownames(vcov)
+      }
+      # - Fixed effects models
+      if(sum(length_ran) == 0){
+        mle <- Rceattle[[i]]$obj$env$last.par.best # Includes fixed and random
+        vcov <- solve(Rceattle[[i]]$sdrep$cov.fixed) # names(mle) == rownames(vcov)
+      }
+      samples <- MASS::mvrnorm(weights[i], mu = mle, Sigma = vcov)
+
+      # - Get quantities
+      quantities <- apply(samples, 1, function(x) Rceattle[[i]]$obj$report(x)[c("biomass", "biomassSSB", "R")]) # Only want uncertainty in SSB, B, and R
+
+      # - Subset years of interest and assign to objects
+      sub_rel_proj_yrs <- (max_styr - Rceattle[[i]]$data_list$styr + 1) : (min_projyr - Rceattle[[i]]$data_list$styr + 1) # Find years of overlap
+
+      # Extract R, B, and SSB and assign to objects
+      recruitment[1:nspp, 1:length(mod_avg_rel_proj_yrs), nrows[which(rowid == i)]] <- unlist(lapply(quantities, function(x) x$R[, sub_rel_proj_yrs]))
+      biomassSSB[1:nspp, 1:length(mod_avg_rel_proj_yrs), nrows[which(rowid == i)]] <- unlist(lapply(quantities, function(x) x$biomassSSB[, sub_rel_proj_yrs]))
+      biomass[1:nspp, 1:length(mod_avg_rel_proj_yrs), nrows[which(rowid == i)]] <- unlist(lapply(quantities, function(x) x$biomass[, sub_rel_proj_yrs]))
+    }
+
+
+    # - Calculate SD
+    rec_rows <- which(names(mod_avg$sdrep$value) == "R")
+    mod_avg$sdrep$sd[rec_rows] <- sqrt(apply(recruitment, c(1,2), var))
+
+    biomass_rows <- which(names(mod_avg$sdrep$value) == "biomass")
+    mod_avg$sdrep$sd[biomass_rows] <- sqrt(apply(biomass, c(1,2), var))
+
+    ssb_rows <- which(names(mod_avg$sdrep$value) == "biomassSSB")
+    mod_avg$sdrep$sd[ssb_rows] <- sqrt(apply(biomassSSB, c(1,2), var))
+
+    # - Save samples
+    mod_avg$asymptotic_samples <- list(recruitment = recruitment, biomass = biomass, biomassSSB = biomassSSB)
   }
 
   return(mod_avg)
