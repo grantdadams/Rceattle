b9b8df1b97424a759b3a520a4891238347860da8
grantdadams <grantadams60091@gmail.com>
2022-03-26 14:50:10 -0700

    cleaned up examples

--- examples/Install_Rceattle.R
+++ examples/Install_Rceattle.R
@@ -1,6 +1,24 @@
-# Steps to install Rceattle from the source file
+# Steps to install Rceattle
 # Grant D. Adams adamsgd@uw.edu
 
+################################################
+# Install from GitHub - Recommended
+################################################
+# Install devtools if you don't already have it
+install.packages("devtools")
+# Install TMB and rtools https://cran.r-project.org/bin/windows/Rtools/
+# Instructions can be found here for non-pc: https://github.com/kaskr/adcomp/wiki/Download
+install.packages('TMB', type = 'source')
+# Try "TMB::runExample(all = TRUE)" to see if TMB works
+devtools::install_github("kaskr/TMB_contrib_R/TMBhelper") # install tmb helper
+
+# Install Rceattle
+devtools::install_github("grantdadams/Rceattle")
+
+
+################################################
+# Install from source
+################################################
 # Step 1 - install compilers
 # Windows install rtools: https://cran.r-project.org/bin/windows/Rtools/
 # Mac install clang and gfortran https://cran.r-project.org/bin/macosx/tools/
@@ -19,7 +37,9 @@ install.packages(c("TMB", "testthat", "reshape2", "oce", "plyr", "readxl", "writ
 
 # Step 5 - set "file_directory" to the directory where the download is
 file_directory <- "your_download_directory"
-path_to_file <- paste(file_directory, "/", "Rceattle_0.0.0.9000.tar.gz", sep = "")
+path_to_file <- paste(file_directory, "/", "Rceattle_1.0.0.0000.tar.gz", sep = "")
 
 # Step 6 - Install Rceattle from source
 install.packages(path_to_file, repos = NULL, type="source", dependencies = TRUE)
+
+
