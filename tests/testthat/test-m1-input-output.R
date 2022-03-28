970f9ff712b085ee826edf806cc0589cfdf7455c
grantdadams <grantadams60091@gmail.com>
2022-03-28 14:47:57 -0700

    Input parameters updated to input M1 base
    Added test to check

---
+++ tests/testthat/test-m1-input-output.R
@@ -0,0 +1,33 @@
+test_that("M1 input and output the same", {
+  data("BS2017SS") # ?BS2017SS for more information on the data
+  data("BS2017MS") # Note: the only difference is the residual mortality (M1_base) is lower
+
+
+  ################################################
+  # Estimation
+  ################################################
+  # - Single species
+  ss_run <- Rceattle::fit_mod(data_list = BS2017SS,
+                              inits = NULL, # Initial parameters = 0
+                              file = NULL, # Don't save
+                              estimateMode = 0, # Estimate
+                              random_rec = FALSE, # No random recruitment
+                              msmMode = 0, # Single species mode
+                              phase = "default",
+                              verbose = 1)
+
+
+  # - Multi species
+  # -- Intialized from single-species MLE parameter values
+  ms_run <- fit_mod(data_list = BS2017MS,
+                    inits = ss_run$estimated_params, # Initial parameters from single species ests
+                    file = NULL, # Don't save
+                    estimateMode = 0, # Estimate
+                    niter = 3, # 3 iterations around population and predation dynamics
+                    random_rec = FALSE, # No random recruitment
+                    msmMode = 1, # MSVPA based
+                    suitMode = 0, # empirical suitability
+                    verbose = 1)
+
+  expect_equal(ms_run$quantities$M1[1,1,1:12], as.numeric(ms_run$data_list$M1_base[1, 3:(12+2)]))
+})
