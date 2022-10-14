684537a3c4e7548ae60bc4b48029640d4cd0d11b
grantdadams <grantadams60091@gmail.com>
2022-10-13 17:05:36 -0700

    Update 10-retrospective_bias.R

--- R/10-retrospective_bias.R
+++ R/10-retrospective_bias.R
@@ -34,7 +34,9 @@ retrospective <- function(Rceattle = NULL, peels = NULL) {
   projyr <- data_list$projyr
   nyrs_proj <- projyr - styr + 1
 
-  # Run across retrospective bits
+  #####################################
+  # Run across retrospective peels
+  #####################################
   ind <- 2
   for (i in 1:peels) {
     data_list$endyr <- endyr - i
@@ -45,90 +47,80 @@ retrospective <- function(Rceattle = NULL, peels = NULL) {
     inits$rec_dev[, (nyrs + 1):nyrs_proj] <- 0
     inits$F_dev <- inits$F_dev[, 1:nyrs]
 
-    inits$ln_srv_q_dev <- inits$ln_srv_q_dev[, 1:nyrs]
-    inits$ln_srv_q_dev_re <- inits$ln_srv_q_dev_re[, 1:nyrs]
-
-    inits$srv_sel_inf_dev <- inits$srv_sel_inf_dev[, ,1:nyrs]
-    inits$srv_sel_inf_dev_re <- inits$srv_sel_inf_dev_re[, ,1:nyrs]
-    inits$srv_sel_slp_dev <- inits$srv_sel_slp_dev[, ,1:nyrs]
-    inits$srv_sel_slp_dev_re <- inits$srv_sel_slp_dev_re[, ,1:nyrs]
-
-    inits$fsh_sel_inf_dev <- inits$fsh_sel_inf_dev[, ,1:nyrs]
-    inits$fsh_sel_inf_dev_re <- inits$fsh_sel_inf_dev_re[, ,1:nyrs]
-    inits$fsh_sel_slp_dev <- inits$fsh_sel_slp_dev[, ,1:nyrs]
-    inits$fsh_sel_slp_dev_re <- inits$fsh_sel_slp_dev_re[, ,1:nyrs]
-
-    # Adjust map parameters
-    map <- Rceattle$map
-    map[[2]]$rec_dev[, (nyrs + 1):nyrs_proj] <- 0
-    map[[2]]$F_dev <- map[[2]]$F_dev[, 1:nyrs]
-
-    map[[2]]$ln_srv_q_dev <- map[[2]]$ln_srv_q_dev[, 1:nyrs]
-    map[[2]]$ln_srv_q_dev_re <- map[[2]]$ln_srv_q_dev_re[, 1:nyrs]
-
-    map[[2]]$srv_sel_inf_dev <- map[[2]]$srv_sel_inf_dev[, ,1:nyrs]
-    map[[2]]$srv_sel_inf_dev_re <- map[[2]]$srv_sel_inf_dev_re[, ,1:nyrs]
-    map[[2]]$srv_sel_slp_dev <- map[[2]]$srv_sel_slp_dev[, ,1:nyrs]
-    map[[2]]$srv_sel_slp_dev_re <- map[[2]]$srv_sel_slp_dev_re[, ,1:nyrs]
-
-    map[[2]]$fsh_sel_inf_dev <- map[[2]]$fsh_sel_inf_dev[, ,1:nyrs]
-    map[[2]]$fsh_sel_inf_dev_re <- map[[2]]$fsh_sel_inf_dev_re[, ,1:nyrs]
-    map[[2]]$fsh_sel_slp_dev <- map[[2]]$fsh_sel_slp_dev[, ,1:nyrs]
-    map[[2]]$fsh_sel_slp_dev_re <- map[[2]]$fsh_sel_slp_dev_re[, ,1:nyrs]
-
-    for (i in 1:length(map[[2]])) {
-      map[[1]][[i]] <- factor(map[[2]][[i]])
-    }
+    # Adjust parameter size
+    inits$ln_srv_q_dev <- inits$ln_srv_q_dev[,1:nyrs]
+    inits$ln_sel_slp_dev <- inits$ln_sel_slp_dev[,,,1:nyrs]
+    inits$sel_inf_dev <- inits$sel_inf_dev[,,,1:nyrs]
+    inits$sel_coff_dev <- array(inits$sel_coff_dev[,,,1:nyrs], dim = c(dim(Rceattle$estimated_params$sel_coff_dev )[1:3], nyrs))
 
     # Refit
-    newmod <- suppressMessages(suppressWarnings(Rceattle::fit_mod(data_list = data_list, inits = inits, file = NULL, debug = 0, map = map,
-                                                                  niter = data_list$niter, random_rec = data_list$random_rec, msmMode = data_list$msmMode, suitMode = data_list$suitMode,
-                                                                  avgnMode = data_list$avgnMode, silent = TRUE)))
+    newmod <- suppressWarnings(
+      Rceattle::fit_mod(
+        data_list = data_list,
+        inits = inits,
+        map =  NULL,
+        bounds = NULL,
+        file = NULL,
+        estimateMode = ifelse(data_list$estimateMode < 3, 0, data_list$estimateMode), # Run hindcast and projection, otherwise debug
+        HCR = build_hcr(HCR = data_list$HCR, # Tier3 HCR
+                        DynamicHCR = data_list$DynamicHCR,
+                        FsprTarget = data_list$FsprTarget,
+                        FsprLimit = data_list$FsprLimit,
+                        Ptarget = data_list$Ptarget,
+                        Plimit = data_list$Plimit,
+                        Alpha = data_list$Alpha,
+                        Pstar = data_list$Pstar,
+                        Sigma = data_list$Sigma
+        ),
+        random_rec = data_list$random_rec,
+        niter = data_list$niter,
+        msmMode = data_list$msmMode,
+        avgnMode = data_list$avgnMode,
+        minNByage = data_list$minNByage,
+        suitMode = data_list$suitMode,
+        phase = NULL,
+        meanyr = data_list$endyr, # Update end year
+        updateM1 = FALSE,
+        getsd = FALSE,
+        verbose = 0)
+
+    )
 
     # Refit model If converged
-    if (!is.null(newmod$opt$opt$Convergence_check)) {
-      if (newmod$opt$opt$Convergence_check != "The model is definitely not converged") {
+    if (!is.null(newmod$opt$Convergence_check)) {
+      if (newmod$opt$Convergence_check != "The model is definitely not converged") {
         mod_list[[ind]] <- newmod
         ind <- ind + 1
       }
     }
   }
 
-    # Calculate Mohs rho for each species
-    objects <- c("biomass", "biomassSSB", "R")
+  #####################################
+  # Calculate Mohs rho for each species
+  #####################################
+  objects <- c("biomass", "biomassSSB", "R", "F_spp")
 
-    mohns <- data.frame(matrix(0, nrow = length(objects) + 1, ncol = 1 + data_list$nspp))
-    colnames(mohns) <- c("Object", paste0("Spp/Fsh_", 1:max(c(data_list$nspp, nrow(data_list$fsh_control)))))
-    mohns$Object <- c(objects, "F")
+  mohns <- data.frame(matrix(0, nrow = length(objects) + 1, ncol = 1 + data_list$nspp))
+  colnames(mohns) <- c("Object", paste0("Spp/Fsh_", 1:max(c(data_list$nspp, nrow(data_list$fsh_control)))))
+  mohns$Object <- c(objects, "F")
 
-    # Loop around peels that converged
-    for (i in 1:(length(mod_list) - 1)) {
-        nyrs_peel <- mod_list[[i + 1]]$data_list$endyr - styr + 1
+  # Loop around peels that converged
+  for (i in 1:(length(mod_list) - 1)) {
+    nyrs_peel <- mod_list[[i + 1]]$data_list$endyr - styr + 1
 
-        # Loop around derived quantitities
-        for (j in 1:length(objects)) {
-            base <- mod_list[[1]]$quantities[[objects[j]]]
-            peel <- mod_list[[i + 1]]$quantities[[objects[j]]]
+    # Loop around derived quantitities
+    for (j in 1:length(objects)) {
+      base <- mod_list[[1]]$quantities[[objects[j]]]
+      peel <- mod_list[[i + 1]]$quantities[[objects[j]]]
 
-            base <- base[, nyrs_peel]
-            peel <- peel[, nyrs_peel]
+      base <- base[, nyrs_peel]
+      peel <- peel[, nyrs_peel]
 
-            rel_error <- ((peel - base)/base)/peels
+      rel_error <- ((peel - base)/base)/peels
 
-            mohns[j, 2:(data_list$nspp + 1) ] <- mohns[j, 2:(data_list$nspp + 1)] + rel_error
-        }
-
-        # F vec
-        base <- exp(mod_list[[1]]$estimated_params$F_dev + mod_list[[1]]$estimated_params$ln_mean_F)
-        peel <- exp(mod_list[[1 + i]]$estimated_params$F_dev + mod_list[[1 + i]]$estimated_params$ln_mean_F)
-
-        base <- base[, nyrs_peel]
-        peel <- peel[, nyrs_peel]
-
-        rel_error <- ((peel - base)/base)/peels
-
-        mohns[nrow(mohns), 2:ncol(mohns) ] <- mohns[nrow(mohns), 2:ncol(mohns) ] + rel_error
+      mohns[j, 2:(data_list$nspp + 1) ] <- mohns[j, 2:(data_list$nspp + 1)] + rel_error
     }
+  }
 
   return(list(Rceattle_list = rev(mod_list), mohns = mohns))
 }
