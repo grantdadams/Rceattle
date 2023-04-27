3753f9fa82736f7c8c50e1ebbfcffc6b5b4d499f
grantdadams <grantadams60091@gmail.com>
2023-04-27 16:10:15 -0700

    Added GOA model example and cleaned up ADREPORT
    Added GOA data and GOA model examples
    Commented out some ADREPORT variables that led to NaN in the sd outputs when there was no issue with SE of parameters

--- examples/Fit_GOA_models.R
+++ examples/Fit_GOA_models.R
@@ -1,7 +1,6 @@
-# Grant Adams, Kirstin Holsman, Andre Punt - April 2019
-# Code to run Bering Sea CEATTLE model in TMB
+# Code to run Gulf of Alaska CEATTLE model in TMB
 # Citation:
-# Holsman, K. K., Ianelli, J., Aydin, K., Punt, A. E., and Moffitt, E. A. 2015. A comparison of fisheries biological reference points estimated from temperature-specific multi-species and single-species climate-enhanced stock assessment models. Deep-Sea Research Part II: Topical Studies in Oceanography, 134: 360–378.
+# Adams, G. D., Holsman, K. K., Barbeaux, S. J., Dorn, M. W., Ianelli, J. N., Spies, I., ... & Punt, A. E. (2022). An ensemble approach to understand predation mortality for groundfish in the Gulf of Alaska. Fisheries Research, 251, 106303.
 
 library(Rceattle)
 
@@ -9,17 +8,9 @@ library(Rceattle)
 # Data
 ################################################
 # Example
-# To run the 2017 single species assessment for the Bering Sea, a data file must first be loaded:
-data("BS2017SS") # Single-species data. ?BS2017SS for more information on the data
-data("BS2017MS") # Multi-species data. Note: the only difference is the residual mortality (M1_base) is lower
-
-# Write data to excel
-Rceattle::write_data(data_list = BS2017SS, file = "BS2017SS.xlsx")
-
-# Change the data how you want in excel
-# Read the data back in
-mydata <- Rceattle::read_data( file = "BS2017SS.xlsx")
-mydata$est_M1 <- c(0,0,0) # Fix M1 to input value (M1_base)
+# To run the 2018 single species assessment for the Gulf of Alaska, a data file must first be loaded:
+data("GOA2018SS") # Single-species data. ?BS2017SS for more information on the data
+GOA2018SS$est_M1 <- c(0,0,0) # Fix M1 to input value (M1_base)
 
 
 ################################################
@@ -27,8 +18,8 @@ mydata$est_M1 <- c(0,0,0) # Fix M1 to input value (M1_base)
 ################################################
 # - Single-species
 # Then the model can be fit by setting `msmMode = 0` using the `Rceattle` function:
-mydata$fleet_control$proj_F_prop <- c(rep(1,3), rep(0,4))
-ss_run <- Rceattle::fit_mod(data_list = mydata,
+GOA2018SS$fleet_control$proj_F_prop <- rep(1, nrow(GOA2018SS$fleet_control))
+ss_run <- Rceattle::fit_mod(data_list = GOA2018SS,
                             inits = NULL, # Initial parameters = 0
                             file = NULL, # Don't save
                             estimateMode = 0, # Estimate
@@ -41,7 +32,7 @@ plot_biomass(ss_run, add_ci = TRUE)
 
 
 # Single-species, but estimate M
-mydata_M <- mydata
+mydata_M <- GOA2018SS
 mydata_M$est_M1 <- c(1,1,1) # Estimate age-invariant M (M2/predation = 0)
 ss_run_M <- Rceattle::fit_mod(data_list = mydata_M,
                               inits = ss_run$estimated_params, # Initial parameters = 0
@@ -54,13 +45,10 @@ ss_run_M <- Rceattle::fit_mod(data_list = mydata_M,
 
 plot_biomass(ss_run_M, add_ci = TRUE)
 
-
 # - Multi-species
 # For the a multispecies model we from the single species parameters.
-BS2017MS$est_M1 <- c(0,0,0) # Do not estimate residual M
-BS2017MS$fleet_control$proj_F_prop <- c(rep(1,3), rep(0,4))
-ms_run <- Rceattle::fit_mod(data_list = BS2017MS,
-                            inits = ss_run$estimated_params, # Initial parameters from single species ests
+ms_run <- Rceattle::fit_mod(data_list = GOA2018SS,
+                            inits = ss_run_M$estimated_params, # Initial parameters from single species ests
                             file = NULL, # Don't save
                             estimateMode = 0, # Estimate
                             niter = 3, # 3 iterations around population and predation dynamics
@@ -70,6 +58,7 @@ ms_run <- Rceattle::fit_mod(data_list = BS2017MS,
                             verbose = 1)
 
 plot_biomass(ms_run, add_ci = TRUE)
+
 
 ################################################
 # Plotting
