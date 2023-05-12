218c99b5d5ae2b9da1bf8334088d18013300d770
grantdadams <grantadams60091@gmail.com>
2023-05-12 10:04:09 -0700

    move to lognormal dist for rec par priors

--- R/dev/BS Rec Fun Tests.R
+++ R/dev/BS Rec Fun Tests.R
@@ -9,14 +9,14 @@ library(dplyr)
 data(BS2017SS) # ?BS2017SS for more information on the data
 BS2017SS$projyr <- 2060
 
-data("BS2017MS") # Note: the only difference is the residual mortality (M1_base) is lower
-BS2017MS$projyr <- 2060
+data("GOA2018SS") # Note: the only difference is the residual mortality (M1_base) is lower
+GOA2018SS$projyr <- 2060
 
 BS2017SS$fleet_control$proj_F_prop <-rep(1,7)
-BS2017MS$fleet_control$proj_F_prop <- rep(1, 7)
+GOA2018SS$fleet_control$proj_F_prop <- rep(1, length(GOA2018SS$fleet_control$proj_F_prop))
 
 
-ss_run_ricker1 <- Rceattle::fit_mod(data_list = BS2017SS,
+ss_run_M_ricker1 <- Rceattle::fit_mod(data_list = BS2017SS,
                                    inits = NULL, # Initial parameters = 0
                                    file = NULL, # Don't save
                                    estimateMode = 1, # Estimate hindcast only
@@ -24,39 +24,42 @@ ss_run_ricker1 <- Rceattle::fit_mod(data_list = BS2017SS,
                                                       proj_mean_rec = FALSE,
                                                       srr_use_prior = TRUE,
                                                       srr_prior_mean = 4,
-                                                      srr_prior_sd = 4),
+                                                      srr_prior_sd = 1),
                                    random_rec = FALSE, # No random recruitment
                                    msmMode = 0, # Single species mode
+                                   M1Fun = build_M1(M1_model = 1),
                                    phase = "default",
                                    verbose = 1,
                                    initMode = 0)
 
-ss_run_ricker2 <- Rceattle::fit_mod(data_list = BS2017SS,
+ss_run_M_ricker2 <- Rceattle::fit_mod(data_list = BS2017SS,
                                    inits = NULL, # Initial parameters = 0
                                    file = NULL, # Don't save
                                    estimateMode = 1, # Estimate hindcast only
                                    recFun = build_srr(srr_fun = 3,
                                                       proj_mean_rec = FALSE,
                                                       srr_use_prior = TRUE,
-                                                      srr_prior_mean = 2,
-                                                      srr_prior_sd = 2),
+                                                      srr_prior_mean = 4,
+                                                      srr_prior_sd = 1),
                                    random_rec = FALSE, # No random recruitment
                                    msmMode = 0, # Single species mode
+                                   M1Fun = build_M1(M1_model = 1),
                                    phase = "default",
                                    verbose = 1,
                                    initMode = 1)
 
-ss_run_ricker3 <- Rceattle::fit_mod(data_list = BS2017SS,
+ss_run_M_ricker3 <- Rceattle::fit_mod(data_list = BS2017SS,
                                    inits = NULL, # Initial parameters = 0
                                    file = NULL, # Don't save
                                    estimateMode = 0, # Estimate hindcast only
                                    recFun = build_srr(srr_fun = 3,
                                                       proj_mean_rec = FALSE,
                                                       srr_use_prior = TRUE,
-                                                      srr_prior_mean = 2,
-                                                      srr_prior_sd = 2),
+                                                      srr_prior_mean = 4,
+                                                      srr_prior_sd = 1),
                                    random_rec = FALSE, # No random recruitment
                                    msmMode = 0, # Single species mode
+                                   M1Fun = build_M1(M1_model = 1),
                                    phase = "default",
                                    verbose = 1,
                                    initMode = 2)
@@ -64,17 +67,53 @@ ss_run_ricker3 <- Rceattle::fit_mod(data_list = BS2017SS,
 
 
 
-data_list = BS2017SS;
-inits = NULL; # Initial parameters = 0
-file = NULL; # Don't save
-estimateMode = 0; # Estimate hindcast only
-recFun = build_srr(srr_fun = 3,
-                   proj_mean_rec = FALSE,
-                   srr_use_prior = TRUE,
-                   srr_prior_mean = 4,
-                   srr_prior_sd = 2);
-random_rec = FALSE; # No random recruitment
-msmMode = 0; # Single species mode
-phase = "default";
-verbose = 1;
-initMode = 2
+ss_run_M_ricker1g <- Rceattle::fit_mod(data_list = GOA2018SS,
+                                    inits = NULL, # Initial parameters = 0
+                                    file = NULL, # Don't save
+                                    estimateMode = 1, # Estimate hindcast only
+                                    recFun = build_srr(srr_fun = 3,
+                                                       proj_mean_rec = FALSE,
+                                                       srr_use_prior = TRUE,
+                                                       srr_prior_mean = 4,
+                                                       srr_prior_sd = 1),
+                                    random_rec = FALSE, # No random recruitment
+                                    msmMode = 0, # Single species mode
+                                    M1Fun = build_M1(M1_model = 1),
+                                    phase = "default",
+                                    verbose = 1,
+                                    initMode = 0)
+
+ss_run_M_ricker2g <- Rceattle::fit_mod(data_list = GOA2018SS,
+                                    inits = NULL, # Initial parameters = 0
+                                    file = NULL, # Don't save
+                                    estimateMode = 1, # Estimate hindcast only
+                                    recFun = build_srr(srr_fun = 3,
+                                                       proj_mean_rec = FALSE,
+                                                       srr_use_prior = TRUE,
+                                                       srr_prior_mean = 4,
+                                                       srr_prior_sd = 1),
+                                    random_rec = FALSE, # No random recruitment
+                                    msmMode = 0, # Single species mode
+                                    M1Fun = build_M1(M1_model = 1),
+                                    phase = "default",
+                                    verbose = 1,
+                                    initMode = 1)
+
+ss_run_M_ricker3g <- Rceattle::fit_mod(data_list = GOA2018SS,
+                                    inits = NULL, # Initial parameters = 0
+                                    file = NULL, # Don't save
+                                    estimateMode = 0, # Estimate hindcast only
+                                    recFun = build_srr(srr_fun = 3,
+                                                       proj_mean_rec = FALSE,
+                                                       srr_use_prior = TRUE,
+                                                       srr_prior_mean = 4,
+                                                       srr_prior_sd = 1),
+                                    random_rec = FALSE, # No random recruitment
+                                    msmMode = 0, # Single species mode
+                                    M1Fun = build_M1(M1_model = 1),
+                                    phase = "default",
+                                    verbose = 1,
+                                    initMode = 2)
+
+
+
