ee9162338314a1bdee7e562bbbb8ca5cb142ced7
grantdadams <grantadams60091@gmail.com>
2023-12-26 15:08:40 -0800

    cppad instead

--- src/TMB/compile.R
+++ src/TMB/compile.R
@@ -4,7 +4,7 @@ tmb_flags <- commandArgs(trailingOnly = TRUE)
 if(file.exists(paste0(tmb_name, ".cpp"))) {
   if(length(tmb_flags) == 0) tmb_flags <- ""
   TMB::compile(file = paste0(tmb_name, ".cpp"),
-               PKG_CXXFLAGS = tmb_flags, framework = "TMBad",
+               PKG_CXXFLAGS = tmb_flags,
                safebounds = FALSE, safeunload = FALSE)
   file.copy(from = paste0(tmb_name, .Platform$dynlib.ext),
             to = "..", overwrite = TRUE)
