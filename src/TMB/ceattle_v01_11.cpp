c55d9836435f252f643fbec7877a1343098a530d
Grant Adams <adamsgd@uw.edu>
2025-05-14 09:22:53 -0700

    Update ceattle_v01_11.cpp

--- src/TMB/ceattle_v01_11.cpp
+++ src/TMB/ceattle_v01_11.cpp
@@ -830,8 +830,8 @@ Type objective_function<Type>::operator() () {
             }
           }
 
-          //Normalize by age rage
-          if(flt_sel_maxage_upper(flt) >= 0){
+          //Normalize by age rage between max lower and max upper
+          if((flt_sel_maxage(flt) >= 0) & (flt_sel_maxage_upper(flt) >= 0)){
             for(age = flt_sel_maxage(flt); age < flt_sel_maxage_upper(flt); age++) {
               max_sel += sel(flt, sex, age, yr)/(flt_sel_maxage_upper(flt) - flt_sel_maxage(flt) + 1);
             }
