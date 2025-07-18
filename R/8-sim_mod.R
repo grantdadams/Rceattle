3d27cd4261a404a1f1307901d1c2260de0cad6ab
Grant Adams <adamsgd@uw.edu>
2025-07-17 18:08:23 -0700

    Moved sample rec to sim_mod
    refactor: In MSE function moved sampling of historical rec devs into seperate function in "8-sim_mod.R"
    fix: added min of srr_year in retro function so that SRRs arent estimated for years beyond hindcast

--- R/8-sim_mod.R
+++ R/8-sim_mod.R
@@ -1,6 +1,127 @@
 #' Simulate Rceattle data
 #'
 #' @description  Simulates data used in Rceattle from the expected values etimated from Rceattle. The variances and uncertainty are the same as used in the operating model. The function currently simulates (assumed distribution) the following: survey biomass (log-normal), survey catch-at-length/age (multinomial), EIT biomass (log-normal), EIT catch-at-length/age (multinomial), total catch (kg) (log-normal), and catch-at-length/age.
+
+
+
+#' Sample historical recruitment deviates and place in the projection
+#'
+#' @param Rceattle
+#' @param sample_rec Include resampled recruitment deviates from the"hindcast" in the projection of the OM. Resampled deviates are used rather than sampling from N(0, sigmaR) because initial deviates bias R0 low. If false, uses mean of recruitment deviates.
+#' @param update_model Update model dynamics. Default = TRUE
+#' @param rec_trend Linear increase or decrease in mean recruitment from \code{endyr} to \code{projyr}. This is the terminal multiplier \code{mean rec * (1 + (rec_trend/projection years) * 1:projection years)}. Can be of length 1 or of length nspp. If length 1, all species get the same trend.
+#'
+#' @returns Rceattle model
+#' @export
+#'
+sample_rec <- function(Rceattle, sample_rec = TRUE, update_model = TRUE, rec_trend = 0){
+
+  # Years for simulations
+  hind_yrs <- (Rceattle$data_list$styr) : Rceattle$data_list$endyr
+  hind_nyrs <- length(hind_yrs)
+  proj_yrs <- (Rceattle$data_list$endyr + 1) : Rceattle$data_list$projyr
+  proj_nyrs <- length(proj_yrs)
+
+  # - Adjust rec trend
+  if(length(rec_trend)==1){
+    rec_trend = rep(rec_trend, Rceattle$data_list$nspp)
+  }
+
+  # Replace future rec devs ----
+  #FIXME - update non-sample rec for stock recruit relationship
+  for(sp in 1:Rceattle$data_list$nspp){
+
+    # -- where SR curve is estimated directly
+    if(Rceattle$data_list$srr_fun == Rceattle$data_list$srr_pred_fun){
+      if(sample_rec){ # Sample devs from hindcast
+        rec_dev <- sample(x = Rceattle$estimated_params$rec_dev[sp, 1:hind_nyrs], size = proj_nyrs, replace = TRUE) + log((1+(rec_trend[sp]/proj_nyrs) * 1:proj_nyrs)) # - Scale mean rec for rec trend
+      } else{ # Set to mean rec otherwise
+        rec_dev <- log(mean(Rceattle$quantities$R[sp,1:hind_nyrs]) * (1+(rec_trend[sp]/proj_nyrs) * 1:proj_nyrs))  - log(Rceattle$quantities$R0[sp]) # - Scale mean rec for rec trend
+      }
+    }
+
+    # -- OMs where SR curve is estimated as penalty (sensu Ianelli)
+    if(Rceattle$data_list$srr_fun != Rceattle$data_list$srr_pred_fun){
+      if(sample_rec){ # Sample devs from hindcast
+        rec_dev <- sample(x = (log(Rceattle$quantities$R) - log(Rceattle$quantities$R_hat))[sp, 1:hind_nyrs],
+                          size = proj_nyrs, replace = TRUE) + log((1+(rec_trend[sp]/proj_nyrs) * 1:proj_nyrs)) # - Scale mean rec for rec trend
+      } else{ # Set to mean rec otherwise
+        rec_dev <- log(mean((log(Rceattle$quantities$R) - log(Rceattle$quantities$R_hat))[sp, 1:hind_nyrs]) * (1+(rec_trend[sp]/proj_nyrs) * 1:proj_nyrs)) # - Scale mean rec for rec trend
+      }
+    }
+
+    # - Update OM with devs
+    Rceattle$estimated_params$rec_dev[sp,proj_yrs - Rceattle$data_list$styr + 1] <- replace(
+      Rceattle$estimated_params$rec_dev[sp,proj_yrs - Rceattle$data_list$styr + 1],
+      values =  rec_dev)
+  }
+
+  if(update_model){
+    # * Update fit ----
+    estMode <- Rceattle$data_list$estimateMode
+    Rceattle <-
+      suppressWarnings(
+        suppressMessages(
+          fit_mod(
+            data_list = Rceattle$data_list,
+            inits = Rceattle$estimated_params,
+            map =  NULL,
+            bounds = NULL,
+            file = NULL,
+            estimateMode = 3,
+            HCR = build_hcr(HCR = Rceattle$data_list$HCR,
+                            DynamicHCR = Rceattle$data_list$DynamicHCR,
+                            Ftarget = Rceattle$data_list$Ftarget,
+                            Flimit = Rceattle$data_list$Flimit,
+                            Ptarget = Rceattle$data_list$Ptarget,
+                            Plimit = Rceattle$data_list$Plimit,
+                            Alpha = Rceattle$data_list$Alpha,
+                            Pstar = Rceattle$data_list$Pstar,
+                            Sigma = Rceattle$data_list$Sigma,
+                            Fmult = Rceattle$data_list$Fmult,
+                            HCRorder = Rceattle$data_list$HCRorder
+            ),
+            recFun = build_srr(srr_fun = Rceattle$data_list$srr_fun,
+                               srr_pred_fun  = Rceattle$data_list$srr_pred_fun,
+                               proj_mean_rec  = Rceattle$data_list$proj_mean_rec,
+                               srr_meanyr = Rceattle$data_list$srr_meanyr,
+                               srr_hat_styr = Rceattle$data_list$srr_hat_styr,
+                               srr_hat_endyr = Rceattle$data_list$srr_hat_endyr,
+                               srr_est_mode  = Rceattle$data_list$srr_est_mode ,
+                               srr_prior  = Rceattle$data_list$srr_prior,
+                               srr_prior_sd   = Rceattle$data_list$srr_prior_sd,
+                               Bmsy_lim = Rceattle$data_list$Bmsy_lim,
+                               srr_indices = Rceattle$data_list$srr_indices),
+            M1Fun =     build_M1(M1_model = Rceattle$data_list$M1_model,
+                                 M1_re = Rceattle$data_list$M1_re,
+                                 updateM1 = FALSE,  # Dont update M1 from data, fix at previous parameters
+                                 M1_use_prior = Rceattle$data_list$M1_use_prior,
+                                 M2_use_prior = Rceattle$data_list$M2_use_prior,
+                                 M_prior = Rceattle$data_list$M_prior,
+                                 M_prior_sd = Rceattle$data_list$M_prior_sd,
+                                 M1_indices = Rceattle$data_list$M1_indices),
+            random_rec = Rceattle$data_list$random_rec,
+            niter = Rceattle$data_list$niter,
+            msmMode = Rceattle$data_list$msmMode,
+            avgnMode = Rceattle$data_list$avgnMode,
+            suitMode = Rceattle$data_list$suitMode,
+            suit_styr = Rceattle$data_list$suit_styr,
+            suit_endyr = Rceattle$data_list$suit_endyr,
+            initMode = Rceattle$data_list$initMode,
+            phase = FALSE,
+            loopnum = Rceattle$data_list$loopnum,
+            getsd = TRUE,
+            verbose = 0)
+        )
+      )
+    Rceattle$data_list$estimateMode <- estMode
+  }
+
+  return(Rceattle)
+}
+
+
+
 #'
 #' @param Rceattle CEATTLE model object exported from \code{\link{Rceattle}}
 #' @param simulate TRUE/FALSE, whether to simulate the data or export the expected value
