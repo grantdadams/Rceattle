59198c5a72af6086ac155c2602b95bc94ee8020b
Grant.Adams <grantadams60091@gmail.com>
2026-02-05 15:35:26 -0800

    Merge branch 'master' into dev-CAAL

--- examples/Install_Rceattle.R
+++ examples/Install_Rceattle.R
@@ -1,47 +1,34 @@
-# Steps to install Rceattle
-# Grant D. Adams adamsgd@uw.edu
 
-################################################
-# Install from GitHub - Recommended
-################################################
+# Install from GitHub ----
+# - Recommended
 # Install devtools if you don't already have it
 install.packages("devtools")
-# Install TMB and rtools https://cran.r-project.org/bin/windows/Rtools/
-# Instructions can be found here for non-pc: https://github.com/kaskr/adcomp/wiki/Download
 
+# Install TMB (instructions: https://github.com/kaskr/adcomp/wiki/Download)
 install.packages('TMB', type = 'source')
+devtools::install_github("kaskr/TMB_contrib_R/TMBhelper") # install tmb helper functions
 # Try "TMB::runExample(all = TRUE)" to see if TMB works
 
-devtools::install_github("kaskr/TMB_contrib_R/TMBhelper") # install tmb helper
+# Install dependencies
+install.packages("pacman")
+pacman::p_load(dplyr,
+               ggplot2,
+               MASS,
+               oce,
+               readxl,
+               TMB,
+               devtools,
+               writexl,
+               reshape2,
+               gplots,
+               tidyr,
+               testthat,
+               foreach,
+               R.utils,
+               knitr,
+               doParallel)
 
 # Install Rceattle
 devtools::install_github("grantdadams/Rceattle")
-
-
-################################################
-# Install from source
-################################################
-# Step 1 - install compilers
-# Windows install rtools: https://cran.r-project.org/bin/windows/Rtools/
-# Mac install clang and gfortran https://cran.r-project.org/bin/macosx/tools/
-# Macs will also need to update makevars file
-
-# Step 2 - Install TMB
-# Instructions can be found here for non-pc: https://github.com/kaskr/adcomp/wiki/Download
-install.packages('TMB', type = 'source')
-# Try "TMB::runExample(all = TRUE)" to see if TMB works
-
-# Step 3 - Install dependencies
-install.packages(c("TMB", "testthat", "reshape2", "oce", "plyr", "readxl", "writexl", "tidyr"))
-
-# Step 4 - Download Rceattle_0.0.0.9000.tar.gz file from:
-# https://drive.google.com/drive/folders/1sHe_KxvKZi7UyjnWB4Oz9HmKY6CWGWoM
-
-# Step 5 - set "file_directory" to the directory where the download is
-file_directory <- "your_download_directory"
-path_to_file <- paste(file_directory, "/", "Rceattle_1.0.0.0000.tar.gz", sep = "")
-
-# Step 6 - Install Rceattle from source
-install.packages(path_to_file, repos = NULL, type="source", dependencies = TRUE)
 
 
