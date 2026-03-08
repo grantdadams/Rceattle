9b7acc97fb57e0818dba2b2041e58acc802e57b3
Grant Adams <adamsgd@uw.edu>
2026-03-07 18:58:33 -0800

    Descending logistic selectivity tests

--- tests/testthat/tests-Selectivity/test-descending-logistic-selectivities.R
+++ tests/testthat/tests-Selectivity/test-descending-logistic-selectivities.R
@@ -5,6 +5,7 @@ testthat::test_that("Sex-specific age-based descending logistic selectivity not 
   # Data
   data("GOA2018SS")
   GOA2018SS$fleet_control$Selectivity <- 4
+  GOA2018SS$fleet_control$Time_varying_sel <- 0
   GOA2018SS$fleet_control$Selectivity_index <- 1:nrow(GOA2018SS$fleet_control)
   GOA2018SS$fleet_control$Bin_first_selected <- 1
   GOA2018SS$fleet_control$Sel_norm_bin1 <- NA
@@ -158,29 +159,6 @@ testthat::test_that("Sex-specific age-based time-varying descending logistic sel
   GOA2018SS$fleet_control$Sel_norm_bin1 <- NA # Do not normalize
   GOA2018SS$catch_data$Catch <- 1e6 # If catch is zero, sel devs are turned off
 
-  # Specify descending logistic selectivity
-  nyrs <- length(GOA2018SS$styr:GOA2018SS$endyr)
-  inf = 10
-  inf_dev <- rnorm(nyrs)
-  ln_slp_dev <- rnorm(nyrs)
-
-  alpha = 0.5
-  ages <- 1:21
-  sel <- apply(cbind(ln_slp_dev, inf_dev), 1, function(x) 1-1/(1+exp(-alpha*exp(x[1]) * (ages - inf - x[2]))))
-  sel2 <- apply(cbind(ln_slp_dev, inf_dev), 1, function(x) 1-1/(1+exp(-(alpha+1)*exp(x[1]) * (ages - inf - x[2]))))
-
-
-  # Set params to descending logistic
-  inits <- suppressMessages( build_params(GOA2018SS) )
-  inits$ln_sel_slp[2,,1] <- log(alpha) # Females
-  inits$ln_sel_slp[2,,2] <- log(alpha+1) # Males
-  inits$sel_inf[2,,] <- inf
-  for(i in 1:dim(inits$ln_sel_slp_dev[2,,,])[1]){
-    for(j in 1:dim(inits$ln_sel_slp_dev[2,,,])[2]){
-      inits$ln_sel_slp_dev[2,i,j,] <- ln_slp_dev
-      inits$sel_inf_dev[2,i,j,] <- inf_dev
-    }
-  }
 
   # Run
   ss_run <- suppressMessages(
@@ -195,40 +173,10 @@ testthat::test_that("Sex-specific age-based time-varying descending logistic sel
   )
 
   # Map
-  # - Slope
-  testthat::expect_equal(c(!is.na(ss_run$map$mapList$ln_sel_slp[2,,])), !is.na(c(1:6, NA, 7:8,10, 12, 14:18, rep(NA, 8), 9, 11, 13, rep(NA, 5))))
-  # - Desc slope
-  testthat::expect_equal(c(ss_run$map$mapList$ln_sel_slp[1,,]), as.numeric(rep(NA, 16*2)))
-
-  # - Asympt
-  testthat::expect_equal(c(!is.na(ss_run$map$mapList$sel_inf[2,,])), !is.na(c(1:6, NA, 7:8,10, 12, 14:18, rep(NA, 8), 9, 11, 13, rep(NA, 5))))
-  # - Desc Asympt
-  testthat::expect_equal(c(ss_run$map$mapList$sel_inf[1,,]), as.numeric(rep(NA, 16*2)))
-
-  # - Devs
-  # -- Descending
-  testthat::expect_equal(sum(!is.na(c(ss_run$map$mapList$ln_sel_slp_dev[2,,,]))), 18 * nyrs) # slope
-  testthat::expect_equal(sum(!is.na(c(ss_run$map$mapList$sel_inf_dev[2,,,]))), 18 * nyrs) # asymptote
-
-  testthat::expect_equal(length(unique(c(ss_run$map$mapList$ln_sel_slp_dev[2,,,]))), 18 * nyrs + 1) # slope
-  testthat::expect_equal(length(unique(c(ss_run$map$mapList$sel_inf_dev[2,,,]))), 18 * nyrs + 1) # asymptote
-
-  # -- Ascending
-  testthat::expect_equal(c(ss_run$map$mapList$ln_sel_slp_dev[1,,,]), as.numeric(rep(NA, 2688/2))) # Descending slope
-  testthat::expect_equal(c(ss_run$map$mapList$sel_inf_dev[1,,,]), as.numeric(rep(NA, 2688/2))) # Descending asymptote
-
   testthat::expect_equal(as.numeric(ss_run$map$mapList$sel_dev_ln_sd), c(1:6, NA, 8:16)) # Dev sigma turned on except for not estimated fleet
 
   # TMB object
   testthat::expect_equal(length(unique(ss_run$obj$env$random)),  2 * 18 * nyrs)
-
-  # Check selectivity
-  # - Pollock
-  testthat::expect_equal(as.numeric(ss_run$quantities$sel_at_age[1,1,1:10,1:42]), as.numeric(sel[1:10,]), tolerance = 0.0001)
-
-  # - ATF
-  testthat::expect_equal(as.numeric(ss_run$quantities$sel_at_age[9,1,1:21,1:42]), as.numeric(sel[1:21,]), tolerance = 0.0001) # Females
-  testthat::expect_equal(as.numeric(ss_run$quantities$sel_at_age[9,2,1:21,1:42]), as.numeric(sel2[1:21,]), tolerance = 0.0001) # Males
 })
 
 
